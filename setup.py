c535cb95fa6aab7f2c31fa7180c8da136fe15a40
Cyrus Afrasiabi <cyrusafrasiabi@Cyruss-MacBook-Pro.local>
2016-03-10 12:46:53 -0800

    Added setup.py, make biothings pip installable.

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="biothings",
+    version="0.0.1",
+    author="Chunlei Wu, Cyrus Afrasiabi",
+    author_email="cwu@scripps.edu",
+    description="Python package for biothings framework",
+    license="BSD",
+    keywords="biology annotation web service client api",
+    url="https://github.com/SuLab/biothings.api",
+    packages=['biothings'],
+    long_description=read('README.rst'),
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Intended Audience :: Science/Research",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    install_requires=[
+        'requests>=2.3.0',
+    ],
+)
