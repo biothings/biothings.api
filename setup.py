b40822204aaf7c922682f40551739840ff3478d6
cyrus0824 <cyrus0824@users.noreply.github.com>
2016-03-16 17:24:21 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     keywords="biology annotation web service client api",
     url="https://github.com/SuLab/biothings.api",
     packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
-    #package_data={'biothings': data_files},
+    package_data={'biothings': data_files},
     #include_package_data=True,
     scripts=['biothings/bin/biothings-admin.py'],
     long_description=read('README.md'),
