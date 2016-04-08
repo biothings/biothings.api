df986adc0231b620688b8c02c4b7bc3805dc0e33
Sebastien Lelong <slelong@scripps.edu>
2016-04-08 11:08:52 -0700

    fix /status endpoint check

--- biothings/www/api/handlers.py
+++ biothings/www/api/handlers.py
@@ -1,5 +1,6 @@
 import re
 import json
+import logging
 from tornado.web import HTTPError
 from biothings.www.helper import BaseHandler
 from biothings.utils.common import split_ids
@@ -202,7 +203,11 @@ class StatusHandler(BaseHandler):
     ''' Handles requests to check the status of the server. '''
 
     def head(self):
+        logging.error("biothing_settings.status_check_id: %s" % biothing_settings.status_check_id)
         r = self.esq.status_check(biothing_settings.status_check_id)
+        if r is None:
+            # we failed to retrieve ref/test doc, something is wrong -> service unavailable
+            raise HTTPError(503)
 
     def get(self):
         self.head()
