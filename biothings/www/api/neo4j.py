0840632b8f5bfbf9937b22cb8a533ba4d1845c93
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2016-05-24 06:27:29 -0700

    Cypher query works now on biothings.

--- biothings/www/api/neo4j.py
+++ biothings/www/api/neo4j.py
@@ -7,12 +7,12 @@ class Neo4jQuery(object):
     def __init__(self):
         pass
 
-    def query(q, **kwargs):
+    def query(self, q, **kwargs):
         # very simple query wrapper
         res = requests.post(biothing_settings.neo4j_host, 
                             auth=(biothing_settings.neo4j_username, biothing_settings.neo4j_password),
                             data = {"query": q, "params": {}})
         if isinstance(res, requests.models.Response) and res.status_code == 200:
-            return rrr.json()
+            return res.json()
         else:
             return {'success': False, 'message': 'Invalid query.'}
