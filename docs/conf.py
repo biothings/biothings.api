2becd63dd35a723e65ecb3c393ec3c1b95c547f6
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2016-07-07 16:24:21 -0700

    Skinning sphinx rtd correctly...

--- docs/conf.py
+++ docs/conf.py
@@ -49,8 +49,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'biothings.api'
-copyright = u'2016, Cyrus Afrasiabi, Chunlei Wu, Sebastien Lelong, Kevin Xin'
-author = u'Cyrus Afrasiabi, Chunlei Wu, Sebastien Lelong, Kevin Xin'
+copyright = u'2016, Su Lab'
+author = u'Su Lab'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -110,7 +110,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -225,7 +225,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   (master_doc, 'biothingsapi.tex', u'biothings.api Documentation',
-   u'Cyrus Afrasiabi, Chunlei Wu, Sebastien Lelong, Kevin Xin', 'manual'),
+   u'Su Lab', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -284,3 +284,12 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+try:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    html_theme_options = {}
+except ImportError:
+    print 'Warning: "sphinx_rtd_theme" is not installed, fall back to default theme.'
+    pass
