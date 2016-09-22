b34833d45822a59f3eba8d5395b3bd0d76b29564
Sebastien Lelong <slelong@scripps.edu>
2016-09-22 11:31:49 -0700

    "dumper" module providing common basic operations to download files split into dumper/upload libraries so biothings.dataload can be imported without doing/requiring anything

---
+++ biothings/dataload/dumper.py
@@ -0,0 +1,236 @@
+import time
+import os
+from datetime import datetime
+
+from biothings.utils.mongo import get_src_dump
+from biothings.utils.common import timesofar
+
+class BaseDumper(object):
+    # override in subclass accordingly
+    SRC_NAME = None
+    SRC_ROOT_FOLDER = None #se  source folder (without version/dates)
+
+    def __init__(self, no_confirm=True, archive=True):
+        self.client = None
+        self.src_dump = None
+        self.src_doc = None
+        self.no_confirm = no_confirm
+        self.archive = archive
+        self.to_dump = []
+        self.release = None
+        self.t0 = time.time()
+        self.logfile = None
+        self.prev_data_folder = None
+        self.timestamp = time.strftime('%Y%m%d')
+        # init
+        self.setup_log()
+        self.prepare()
+
+    def create_todump_list(self,force=False):
+        """Fill self.to_dump list with dict("remote":remote_path,"local":local_path)
+        elements. This is the todo list for the dumper. It's a good place to
+        check whether needs to be downloaded. If 'force' is True though, all files
+        will be considered for download"""
+        raise NotImplementedError("Define in subclass")
+
+    def prepare_client(self):
+        """do initialization to make the client ready to dump files"""
+        raise NotImplementedError("Define in subclass")
+
+    def need_prepare(self):
+        """check whether some prepare step should executed
+        before running dump"""
+
+    def release_client(self):
+        """Do whatever necessary (like closing network connection)
+        to "release" the client"""
+        raise NotImplementedError("Define in subclass")
+
+    def remote_is_better(self,remotefile,localfile):
+        '''Compared to local file, check if remote file is worth downloading.
+        (like either bigger or newer for instance)'''
+        raise NotImplementedError("Define in subclass")
+
+    def download(self,remotefile,localfile):
+        """Download "remotefile' to local location defined by 'localfile'"""
+        raise NotImplementedError("Define in subclass")
+
+    def setup_log(self):
+        import logging as logging_mod
+        if not os.path.exists(self.SRC_ROOT_FOLDER):
+            os.makedirs(self.SRC_ROOT_FOLDER)
+        self.logfile = os.path.join(self.SRC_ROOT_FOLDER, '%s_%s_dump.log' % (self.SRC_NAME,self.timestamp))
+        fh = logging_mod.FileHandler(self.logfile)
+        fh.setFormatter(logging_mod.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+        fh.name = "logfile"
+        sh = logging_mod.StreamHandler()
+        sh.name = "logstream"
+        logger = logging_mod.getLogger("%s_dump" % self.SRC_NAME)
+        logger.setLevel(logging_mod.DEBUG)
+        if not fh.name in [h.name for h in logger.handlers]:
+            logger.addHandler(fh)
+        if not sh.name in [h.name for h in logger.handlers]:
+            logger.addHandler(sh)
+        # propagate to global "logging" var for convenience
+        global logging
+        logging = logger
+
+    def prepare(self):
+        self.prepare_client()
+        self.prepare_src_dump()
+        self.new_data_folder = self.get_new_data_folder()
+        self.current_data_folder = self.src_doc.get("data_folder")
+        self.to_dump = []
+
+    def prepare_src_dump(self):
+        # Mongo side
+        self.src_dump = get_src_dump()
+        self.src_doc = self.src_dump.find_one({'_id': self.SRC_NAME}) or {}
+
+    def register_status(self,status,transient=False,**extra):
+        self.src_doc = {'_id': self.SRC_NAME,
+               'timestamp': self.timestamp,
+               'data_folder': self.new_data_folder,
+               'release': self.release,
+               'logfile': self.logfile,
+               'status': status}
+        # only register time when it's a final state
+        if not transient:
+            self.src_doc["time"] = timesofar(self.t0)
+        self.src_doc.update(extra)
+        self.src_dump.save(self.src_doc)
+
+    def dump(self,force=False):
+        '''
+        Dump (ie. download) resource as needed
+        this should be called after instance creation
+        'force' argument will force dump, passing this to
+        create_todump_list() method.
+        '''
+        try:
+            if self.need_prepare():
+                self.prepare()
+            self.create_todump_list(force=force)
+            if self.to_dump:
+                # mark the download starts
+                self.register_status("downloading",transient=True)
+                self.do_dump()
+                self.register_status("success",pending_to_upload=True)
+        except (KeyboardInterrupt,Exception) as e:
+            logging.error("Error while dumping source: %s" % e)
+            import traceback
+            logging.error(traceback.format_exc())
+            self.register_status("failed")
+        finally:
+            if self.client:
+                self.release_client()
+
+    def get_new_data_folder(self):
+        if self.archive:
+            return os.path.join(self.SRC_ROOT_FOLDER, self.timestamp)
+        else:
+            return os.path.join(self.SRC_ROOT_FOLDER, 'latest')
+
+    def do_dump(self):
+        logging.info("%d files to download" % len(self.to_dump))
+        for todo in self.to_dump:
+            remote = todo["remote"]
+            local = todo["local"]
+            self.download(remote,local)
+
+    def prepare_local_folders(self,localfile):
+        localdir = os.path.dirname(localfile)
+        if not os.path.exists(localdir):
+            os.makedirs(localdir)
+
+
+
+from ftplib import FTP
+
+class FTPDumper(BaseDumper):
+    FTP_HOST = 'ftp.ncbi.nlm.nih.gov'
+    CWD_DIR = '/snp/organisms'
+
+    def prepare_client(self):
+        # FTP side
+        self.client = FTP(self.FTP_HOST)
+        self.client.login()
+        if self.CWD_DIR:
+            self.client.cwd(self.CWD_DIR)
+
+    def need_prepare(self):
+        return not self.client or (self.client and not self.client.file)
+
+    def release_client(self):
+        assert self.client
+        self.client.close()
+
+    def download(self,remotefile,localfile):
+        self.prepare_local_folders(localfile)
+        logging.debug("Downloading '%s'" % remotefile)
+        with open(localfile,"wb") as out_f:
+            self.client.retrbinary('RETR %s' % remotefile, out_f.write)
+        # set the mtime to match remote ftp server
+        response = self.client.sendcmd('MDTM ' + remotefile)
+        code, lastmodified = response.split()
+        # an example: 'last-modified': '20121128150000'
+        lastmodified = time.mktime(datetime.strptime(lastmodified, '%Y%m%d%H%M%S').timetuple())
+        os.utime(localfile, (lastmodified, lastmodified))
+
+    def remote_is_better(self,remotefile,localfile):
+        """'remotefile' is relative path from current working dir (CWD_DIR), 
+        'localfile' is absolute path"""
+        res = os.stat(localfile)
+        local_lastmodified = int(res.st_mtime)
+        response = self.client.sendcmd('MDTM ' + remotefile)
+        code, remote_lastmodified = response.split()
+        remote_lastmodified = int(time.mktime(datetime.strptime(remote_lastmodified, '%Y%m%d%H%M%S').timetuple()))
+
+        if remote_lastmodified > local_lastmodified:
+            logging.debug("Remote file '%s' is newer (remote: %s, local: %s)" %
+                    (remotefile,remote_lastmodified,local_lastmodified))
+            return True
+        local_size = res.st_size
+        self.client.sendcmd("TYPE I")
+        response = self.client.sendcmd('SIZE ' + remotefile)
+        code, remote_size= map(int,response.split())
+        if remote_size > local_size:
+            logging.debug("Remote file '%s' is bigger (remote: %s, local: %s)" % (remotefile,remote_size,local_size))
+            return True
+        logging.debug("'%s' is up-to-date, no need to download" % remotefile)
+        return False
+
+
+class WgetDumper(BaseDumper):
+
+    def create_todump_list(self,force=False):
+        """Fill self.to_dump list with dict("remote":remote_path,"local":local_path)
+        elements. This is the todo list for the dumper. It's a good place to
+        check whether needs to be downloaded. If 'force' is True though, all files
+        will be considered for download"""
+        raise NotImplementedError("Define in subclass")
+
+    def prepare_client(self):
+        """Check if 'wget' executable exists"""
+        ret = os.system("type wget 2>&1 > /dev/null")
+        if not ret == 0:
+            raise Exception("Can't find wget executable")
+
+    def need_prepare(self):
+        return False
+
+    def release_client(self):
+        pass
+
+    def remote_is_better(self,remotefile,localfile):
+        return True
+
+    def download(self,remoteurl,localfile):
+        self.prepare_local_folders(localfile)
+        cmdline = "wget %s -O %s" % (remoteurl, localfile)
+        return_code = os.system(cmdline)
+        if return_code == 0:
+            logging.info("Success.")
+        else:
+            logging.error("Failed with return code (%s)." % return_code)
+
---
+++ biothings/dataload/uploader.py
@@ -0,0 +1,163 @@
+import time, sys, os, importlib, copy
+import datetime, types
+from biothings.utils.common import get_timestamp, get_random_string, timesofar, iter_n
+from biothings.utils.mongo import get_src_conn
+
+
+class SourceUploader(object):
+    __sources__ = {}
+
+    def __init__(self,sources):
+        self.__class__.__sources__ = sources
+        self.doc_register = {}
+        self.conn = get_src_conn()
+
+    def register_sources(self):
+        for src in self.__sources__:
+            src_m = importlib.import_module('dataload.sources.' + src)
+            metadata = src_m.__metadata__
+            name = src + '_doc'
+            metadata['load_data'] = src_m.load_data
+            metadata['get_mapping'] = src_m.get_mapping
+            metadata['conn'] = self.conn
+            src_cls = type(name, (DocSource,), metadata)
+            # manually propagate db attr
+            src_cls.db = self.conn[src_cls.__database__]
+            self.doc_register[name] = src_cls
+            self.conn.register(src_cls)
+
+    def upload_all(self,**kwargs):
+        for src in self.__sources__:
+            print("src: %s" % src)
+            self.upload_src(src, **kwargs)
+
+    def upload_src(self, src, **kwargs):
+        _src = self.doc_register[src + '_doc']()
+        _src.load(**kwargs)
+
+
+class DocSourceMaster(dict):
+    '''A class to manage various doc data sources.'''
+    # TODO: fix this delayed import
+    from biothings import config
+    __collection__ = config.DATA_SRC_MASTER_COLLECTION
+    __database__ = config.DATA_SRC_DATABASE
+    use_dot_notation = True
+    use_schemaless = True
+    structure = {
+        'name': str,
+        'timestamp': datetime.datetime,
+    }
+
+class DocSource(dict):
+    # TODO: fix this delayed import
+    from biothings import config
+    __collection__ = None      # should be specified individually
+    __database__ = config.DATA_SRC_DATABASE
+    temp_collection = None     # temp collection is for dataloading
+
+    def make_temp_collection(self):
+        '''Create a temp collection for dataloading, e.g., entrez_geneinfo_INEMO.'''
+        new_collection = None
+        while 1:
+            new_collection = self.__collection__ + '_temp_' + get_random_string()
+            if new_collection not in self.db.collection_names():
+                break
+        self.temp_collection = self.db[new_collection]
+        return new_collection
+
+    def switch_collection(self):
+        '''after a successful loading, rename temp_collection to regular collection name,
+           and renaming existing collection to a temp name for archiving purpose.
+        '''
+        if self.temp_collection and self.temp_collection.count() > 0:
+            if self.collection.count() > 0:
+                # renaming existing collections
+                new_name = '_'.join([self.__collection__, 'archive', get_timestamp(), get_random_string()])
+                self.collection.rename(new_name, dropTarget=True)
+            self.temp_collection.rename(self.__collection__)
+        else:
+            print("Error: load data first.")
+
+    def doc_iterator(self, doc_d, batch=True, step=10000):
+        if isinstance(doc_d, types.GeneratorType) and batch:
+            for doc_li in iter_n(doc_d, n=step):
+                yield doc_li
+        else:
+            if batch:
+                doc_li = []
+                i = 0
+            for _id, doc in doc_d.items():
+                doc['_id'] = _id
+                _doc = copy.copy(self)
+                _doc.clear()
+                _doc.update(doc)
+                #if validate:
+                #    _doc.validate()
+                if batch:
+                    doc_li.append(_doc)
+                    i += 1
+                    if i % step == 0:
+                        yield doc_li
+                        doc_li = []
+                else:
+                    yield _doc
+
+            if batch:
+                yield doc_li
+
+    def post_update_data(self):
+        """Override as needed to perform operations after
+           date has been uploaded"""
+        pass
+
+    def update_data(self, doc_d, step):
+        doc_d = doc_d or self.load_data()
+        print("doc_d mem: %s" % sys.getsizeof(doc_d))
+
+        print("Uploading to the DB...", end='')
+        t0 = time.time()
+        for doc_li in self.doc_iterator(doc_d, batch=True, step=step):
+            self.temp_collection.insert(doc_li, manipulate=False, check_keys=False)
+        print('Done[%s]' % timesofar(t0))
+        self.switch_collection()
+        self.post_update_data()
+
+    def generate_doc_src_master(self):
+        _doc = {"_id": str(self.__collection__),
+                "name": str(self.__collection__),
+                "timestamp": datetime.datetime.now()}
+        if hasattr(self, 'get_mapping'):
+            _doc['mapping'] = getattr(self, 'get_mapping')()
+        return _doc
+
+    def update_master(self):
+        _doc = self.generate_doc_src_master()
+        self.save_doc_src_master(_doc)
+
+    def save_doc_src_master(self,_doc):
+        coll = self.conn[DocSourceMaster.__database__][DocSourceMaster.__collection__]
+        dkey = {"_id": _doc["_id"]}
+        prev = coll.find_one(dkey)
+        if prev:
+            coll.replace_one(dkey, _doc)
+        else:
+            coll.insert_one(_doc)
+
+    def load(self, doc_d=None, update_data=True, update_master=True, test=False, step=10000):
+        if not self.temp_collection:
+            self.make_temp_collection()
+        self.temp_collection.drop()       # drop all existing records just in case.
+
+        if update_data:
+            self.update_data(doc_d,step)
+        if update_master:
+            # update src_master collection
+            if not test:
+                self.update_master()
+
+    @property
+    def collection(self):
+        return self.db[self.__collection__]
+
+
