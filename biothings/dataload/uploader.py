433f9f6f72e5815d2120219e528318fb2135a57f
Sebastien Lelong <slelong@scripps.edu>
2016-10-03 15:34:31 -0700

    created some esource exceptions + merger uploader (for mem-optimized uploaders)

--- biothings/dataload/uploader.py
+++ biothings/dataload/uploader.py
@@ -1,15 +1,21 @@
 import time, sys, os, importlib, copy
-import datetime, types
+import datetime, types, pprint
 
-from pymongo.errors import DuplicateKeyError
+from pymongo.errors import DuplicateKeyError, BulkWriteError
 
 from biothings.utils.common import get_timestamp, get_random_string, timesofar, iter_n
 from biothings.utils.mongo import get_src_conn, get_src_dump
+from biothings.utils.dataload import merge_struct
+
 from biothings import config
 
 logging = config.logger
 
 class ResourceNotReady(Exception):
+    pass
+class UnknownResource(Exception):
+    pass
+class ResourceError(Exception):
     pass
 
 class DocSourceMaster(dict):
@@ -36,8 +42,10 @@ class BaseSourceUploader(object):
     temp_collection = None     # temp collection is for dataloading
 
     # Will be override in subclasses
-    name = None
-    main_source =None
+    name = None # name of the resource and collection name used to store data
+    main_source =None # if several resources, this one if the main name,
+                      # it's also the _id of the resource in src_dump collection
+                      # if set to None, it will be set to the value of variable "name"
 
     def __init__(self,db_conn,data_root):
         """db_conn is a database connection to fetch/store information about the datasource's state
@@ -53,12 +61,17 @@ class BaseSourceUploader(object):
         self.setup_log()
 
     def check_ready(self):
+        if not self.src_doc:
+            raise ResourceNotReady("Missing information for source '%s' to start upload" % self.main_source)
         if not self.src_doc.get("data_folder"):
-            raise ResourceNotReady("No data folder found")
+            raise ResourceNotReady("No data folder found for resource '%s'" % self.name)
         if not self.src_doc.get("download",{}).get("status") == "success":
-            raise ResourceNotReady("No successful download found, src_doc: %s" % self.src_doc)
+            raise ResourceNotReady("No successful download found for resource '%s'" % self.name)
         if not os.path.exists(self.src_root_folder):
-            raise ResourceNotReady("Data folder '%s' doesn't exist")
+            raise ResourceNotReady("Data folder '%s' doesn't exist for resource '%s'" % self.name)
+        if self.src_doc.get("upload",{}).get("status") == "uploading":
+            pid = self.src_doc.get("upload",{}).get("pid","unknown")
+            raise ResourceNotReady("Resource '%s' is already being uploaded (pid: %s)" % (self.name,pid))
 
     def load_data(self,data_folder):
         """Parse data inside data_folder and return structure ready to be
@@ -90,7 +103,7 @@ class BaseSourceUploader(object):
                 self.collection.rename(new_name, dropTarget=True)
             self.temp_collection.rename(self.name)
         else:
-            self.logger.error("Error: load data first.")
+            raise ResourceError("No temp collection (or it's empty)")
 
     def doc_iterator(self, doc_d, batch=True, step=10000):
         if isinstance(doc_d, types.GeneratorType) and batch:
@@ -102,8 +115,8 @@ class BaseSourceUploader(object):
                 i = 0
             for _id, doc in doc_d.items():
                 doc['_id'] = _id
-                _doc = copy.copy(self)
-                _doc.clear()
+                _doc = {}
+                #_doc.clear()
                 _doc.update(doc)
                 #if validate:
                 #    _doc.validate()
@@ -139,8 +152,12 @@ class BaseSourceUploader(object):
         _doc = {"_id": str(self.name),
                 "name": str(self.name), # TODO: remove ?
                 "timestamp": datetime.datetime.now()}
+        # store mapping
         if hasattr(self, 'get_mapping'):
             _doc['mapping'] = getattr(self, 'get_mapping')()
+        # type of id being stored in these docs
+        if hasattr(self, 'id_type'):
+            _doc['id_type'] = getattr(self, 'id_type')
         return _doc
 
     def update_master(self):
@@ -158,7 +175,7 @@ class BaseSourceUploader(object):
 
     def register_status(self,status,transient=False,**extra):
         upload_info = {
-                'started_at': self.timestamp,
+                'started_at': datetime.datetime.now(),
                 'logfile': self.logfile,
                 'status': status}
 
@@ -188,6 +205,7 @@ class BaseSourceUploader(object):
 
     def load(self, doc_d=None, update_data=True, update_master=True, test=False, step=10000, progress=None, total=None):
         # sanity check before running
+        self.logger.info("Upoading '%s'" % self.name)
         self.check_ready()
         try:
             self.unset_pending_upload()
@@ -219,7 +237,6 @@ class BaseSourceUploader(object):
     def prepare_src_dump(self):
         self.src_dump = get_src_dump()
         self.src_doc = self.src_dump.find_one({'_id': self.main_source})
-        assert self.src_doc, "Missing information for source '%s' to start upload" % self.main_source
 
     def setup_log(self):
         import logging as logging_mod
@@ -294,7 +311,7 @@ class SourceStorage(object):
                 logging.debug("Found uploader class '%s'" % uploader_klass)
                 break
         if not uploader_klass:
-            raise AttributeError("Can't find an uploader class in module '%s'" % src_module)
+            raise UnknownResource("Can't find an uploader class in module '%s'" % src_module)
         uploader_inst = uploader_klass(
                 db_conn=self.conn,
                 data_root=config.DATA_ARCHIVE_ROOT
@@ -312,6 +329,7 @@ class SourceStorage(object):
                 src_m = importlib.import_module(src_data)
             except ImportError:
                 try:
+                    print("onela %s.%s" % (self.default_src_path,src_data))
                     src_m = importlib.import_module("%s.%s" % (self.default_src_path,src_data))
                 except ImportError:
                     logging.error("Can't find module '%s', even in '%s'" % (src_data,self.default_src_path))
@@ -333,12 +351,23 @@ class SourceStorage(object):
         self.conn.register(uploader_inst.__class__)
 
     def register_sources(self, sources):
+        assert not isinstance(sources,str), "sources argument is a string, should pass a list"
+        self.doc_register.clear()
         for src_data in sources:
             self.register_source(src_data)
 
-    def upload_all(self,**kwargs):
+    def upload_all(self,raise_on_error=False,**kwargs):
+        errors = {}
         for src in self.doc_register:
-            self.upload_src(src, **kwargs)
+            try:
+                self.upload_src(src, **kwargs)
+            except Exception as e:
+                errors[src] = e
+                if raise_on_error:
+                    raise
+        if errors:
+            logging.warning("Found errors while uploading:\n%s" % pprint.pformat(errors))
+            return errors
 
     def upload_src(self, src, **kwargs):
         uploaders = None
@@ -355,18 +384,83 @@ class SourceStorage(object):
                         logging.info("Found uploader '%s' for '%s'" % (uploaders,src))
                         break
         if not uploaders:
-            raise ValueError("Can't find '%s' in registered sources (whether as main or sub-source)" % src)
+            raise ResourceError("Can't find '%s' in registered sources (whether as main or sub-source)" % src)
 
-        if isinstance(uploaders,list):
-            # this is a resource composed by several sub-resources
-            # let's mention intermediate step (so "success" means all subsources
-            # have been uploaded correctly
-            for i,one in enumerate(uploaders):
-                one.load(progress=i+1,total=len(uploaders),**kwargs)
-        else:
-            uploader = uploaders # for the sake of readability...
-            uploader.load(**kwargs)
+        try:
+            if isinstance(uploaders,list):
+                # this is a resource composed by several sub-resources
+                # let's mention intermediate step (so "success" means all subsources
+                # have been uploaded correctly
+                for i,one in enumerate(uploaders):
+                    one.load(progress=i+1,total=len(uploaders),**kwargs)
+            else:
+                uploader = uploaders # for the sake of readability...
+                uploader.load(**kwargs)
+        except Exception as e:
+            logging.error("Error while uploading '%s': %s" % (src,e))
+            raise
 
     def __repr__(self):
         return "<%s [%d registered]: %s>" % (self.__class__.__name__,len(self.doc_register), list(self.doc_register.keys()))
 
+
+class MergerSourceUploader(BaseSourceUploader):
+    """
+    This uploader will try to merge documents when finding duplicated errors.
+    It's useful when data is parsed using iterator. A record can be stored in database,
+    then later, another record with the same ID is sent to the db, raising a duplicated error.
+    These two documents would have been merged before using a 'put all in memory' parser. 
+    Since data is here read line by line, the merge is done while storing
+    """
+
+    def update_data(self, doc_d, step):
+        doc_d = doc_d or self.load_data(data_folder=self.data_folder)
+        self.logger.info("Uploading to the DB...")
+        t0 = time.time()
+        tinner = time.time()
+        aslistofdict = None
+        for doc_li in self.doc_iterator(doc_d, batch=True, step=step):
+            toinsert = len(doc_li)
+            nbinsert = 0
+            self.logger.info("Inserting %s records ... " % toinsert)
+            try:
+                bob = self.temp_collection.initialize_unordered_bulk_op()
+                for d in doc_li:
+                    aslistofdict = d.pop("__aslistofdict__",None)
+                    bob.insert(d)
+                res = bob.execute()
+                nbinsert += res["nInserted"]
+                self.logger.info("OK [%s]" % timesofar(tinner))
+            except BulkWriteError as e:
+                inserted = e.details["nInserted"]
+                nbinsert += inserted
+                self.logger.info("Fixing %d records " % len(e.details["writeErrors"]))
+                ids = [d["op"]["_id"] for d in e.details["writeErrors"]]
+                # build hash of existing docs
+                docs = self.temp_collection.find({"_id" : {"$in" : ids}})
+                hdocs = {}
+                for doc in docs:
+                    hdocs[doc["_id"]] = doc
+                bob2 = self.temp_collection.initialize_unordered_bulk_op()
+                for err in e.details["writeErrors"]:
+                    errdoc = err["op"]
+                    existing = hdocs[errdoc["_id"]]
+                    assert "_id" in existing
+                    _id = errdoc.pop("_id")
+                    merged = merge_struct(errdoc, existing,aslistofdict=aslistofdict)
+                    bob2.find({"_id" : _id}).update_one({"$set" : merged})
+                    # update previously fetched doc. if several errors are about the same doc id,
+                    # we would't merged things properly without an updated document
+                    assert "_id" in merged
+                    hdocs[_id] = merged
+                    nbinsert += 1
+
+                res = bob2.execute()
+                self.logger.info("OK [%s]" % timesofar(tinner))
+            assert nbinsert == toinsert, "nb %s to %s" % (nbinsert,toinsert)
+            # end of loop so it counts the time spent in doc_iterator
+            tinner = time.time()
+
+        self.logger.info('Done[%s]' % timesofar(t0))
+        self.switch_collection()
+        self.post_update_data()
