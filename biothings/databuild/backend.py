bc072e1106a12c5bd689e1bbebc964f0bf2011ca
Sebastien Lelong <slelong@scripps.edu>
2016-10-27 16:07:11 -0700

    restore some backends specific to build/merge step

--- biothings/databuild/backend.py
+++ biothings/databuild/backend.py
@@ -54,4 +54,43 @@ class TargetDocBackend(DocBackendBase):
     def post_merge(self):
         pass
 
+class SourceDocMongoBackend(SourceDocBackendBase):
+
+    def get_build_configuration(self, build):
+        self._build_config = self.build.find_one({'_id' : build})
+        return self._build_config
+
+    def validate_sources(self,sources=None):
+        assert self._build_config, "'self._build_config' cannot be empty."
+        if self.src_masterdocs is None:
+            self.src_masterdocs = self.get_src_master_docs()
+        if not sources:
+            sources = set(self.sources.collection_names())
+            build_conf_src = self._build_config['sources']
+        else:
+            build_conf_src = sources
+        # check interseciton between what's needed and what's existing
+        for src in build_conf_src:
+            assert src in self.src_masterdocs, '"%s" not found in "src_master"' % src
+            assert src in sources, '"%s" not an existing collection in "%s"' % (src, self.sources.name)
+
+    def get_src_master_docs(self):
+        if self.src_masterdocs is None:
+            self.src_masterdocs = dict([(src['_id'], src) for src in list(self.master.find())])
+        return self.src_masterdocs
+
+    def get_src_versions(self):
+        src_version = {}
+        for src in self.dump.find():
+            version = src.get('release', src.get('timestamp', None))
+            if version:
+                src_version[src['_id']] = version
+        return src_version
+
+
+class TargetDocMongoBackend(TargetDocBackend,DocMongoBackend):
+
+    def set_target_name(self,target_name=None, build_name=None):
+        super(TargetDocMongoBackend,self).set_target_name(target_name,build_name)
+        self.target_collection = self.target_db[self.target_name]
 
