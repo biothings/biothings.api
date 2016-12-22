1bad58a73255e28e51bb03a69a46f375cbd705f7
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2016-12-21 22:05:33 -0800

    Adding argparse to sort command line options, adding a backend option to match the new template folder organization.

--- biothings/bin/biothings-admin.py
+++ biothings/bin/biothings-admin.py
@@ -1,66 +1,84 @@
 #!/usr/bin/env python
 import os
-import re
-import sys
 import biothings
+import argparse
 from string import Template
-from shutil import copytree
 
-def usage():
-    return "Usage: biothings-admin.py < path to project directory > < object name > < OPTIONAL key=value pairs to override template variables >"
+def log(msg):
+    print("\n******************")
+    print(msg)
+    print("******************\n")
+    return
 
 def main(args):
-    try:
-        pdir = os.path.abspath(args[1])
-        biothing_name = args[2]
-        clargs = dict([arg.split('=', maxsplit=1) for arg in sys.argv[3:]])
-    except:
-        print("Valid project destination directory and object name must be included.")
-        print(usage())
-        sys.exit(1)
+    # now only es is supported as a biothing backend...
+    if args.b == 'es':
+        subdir = 'es_project_template'
+    else:
+        pass
 
-    cwd = os.getcwd()
-    template_dir = os.path.join(os.path.split(biothings.__file__)[0], 'conf', 'biothings_templates')
+    # get the template dir for the proper backend
+    template_dir = os.path.join(os.path.split(biothings.__file__)[0], 'conf', 'biothings_templates', subdir)
 
-    if not os.path.exists(template_dir):
-        print("Could not find template directory.  Exiting.")
-        sys.exit(1)
+    if args.verbose:
+        log("Template directory: {}".format(template_dir))
 
-    # create settings dict
+    # generate template settings
     settings_dict = {
-        "src_package": 'my' + biothing_name.lower(),
-        "settings_class": "My" + biothing_name.title() + "Settings",
-        "annotation_endpoint": biothing_name.lower(),
+        "src_package": 'my' + args.obj.lower(),
+        "settings_class": "My" + args.obj.title() + "Settings",
+        "annotation_endpoint": args.obj.lower(),
         "query_endpoint": "query",
-        "annotation_handler_name": biothing_name.title() + "Handler",
+        "annotation_handler_name": args.obj.title() + "Handler",
         "query_handler_name": "QueryHandler",
-        "es_doctype": biothing_name.lower(),
-        "base_url": "My" + biothing_name.title() + ".info",
-        "nosetest_settings_class": biothing_name.title(),
-        "nosetest_envar": "M" + biothing_name.upper()[0] + '_HOST'
+        "es_doctype": args.obj.lower(),
+        "base_url": "My" + args.obj.title() + ".info",
+        "nosetest_settings_class": args.obj.title(),
+        "nosetest_envar": "M" + args.obj.upper()[0] + '_HOST'
     }
+
+    if args.l:
+        log("List of available settings:\n{}".format(list(settings_dict.keys())))
+        return
+
+    # store cwd
+    cwd = os.getcwd()
+
+    # make command line args into dict
+    clargs = dict([i.split('=', maxsplit=1) for i in args.o])
 
     # override any key value pairs from the command line
     settings_dict.update(clargs)
+
+    if args.verbose:
+        log("Creating {} project template using {}".format(args.b, settings_dict))
     
     # Make top level directory
-    pdir = os.path.join(pdir, settings_dict["src_package"])
+    pdir = os.path.join(os.path.abspath(args.path), settings_dict["src_package"])
+    if args.verbose:
+        log("Creating directory structure . . . {}".format(pdir))
     os.mkdir(pdir)
 
     # Start to template files out
     os.chdir(template_dir)
 
     # Template files out
-    for (index, (dirpath, dirnames, filenames)) in enumerate(list(os.walk('src'))):
+    for (index, (dirpath, dirnames, filenames)) in enumerate(list(os.walk('.'))):
         thisdir = os.path.join(pdir, dirpath)
         os.mkdir(thisdir)
-        for fi in filenames:
-            f = open(os.path.join(thisdir, fi.rstrip('-tpl')), 'w')
-            g = open(os.path.join(os.path.abspath(dirpath), fi), 'r')
-            f.write(Template(g.read()).substitute(settings_dict))
-            f.close()
-            g.close()
+        for fi in [f for f in filenames if f.endswith('-tpl')]:
+            with open(os.path.join(thisdir, fi[:-4]), 'w') as outfile, open(
+                os.path.join(os.path.abspath(dirpath), fi), 'r') as infile:
+                outfile.write(Template(infile.read()).substitute(settings_dict))
     os.chdir(cwd)
 
 if __name__ == '__main__':
-    main(sys.argv)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('obj', help="biothing type (e.g. gene, variant, drug, etc).  Default settings are created from this")
+    parser.add_argument('path', help="path where skeleton project should be placed")
+    parser.add_argument('-o', default=[], nargs="*", help="override any of the default settings with key=value pairs, to see default settings list, use -l")
+    parser.add_argument('-l', default=False, dest="l", action="store_true")
+    parser.add_argument('-b', default='es', help="type of database used for this biothing project")
+    parser.add_argument('-v', default=False, dest="verbose", action="store_true")
+    args = parser.parse_args()
+    main(args)
