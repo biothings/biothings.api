39ed6a1611b309a8e78c0f9a776dbc9413c96b2b
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2017-01-01 07:59:01 -0800

    New settings structure

--- biothings/www/settings/__init__.py
+++ biothings/www/settings/__init__.py
@@ -1,198 +1,53 @@
 # -*- coding: utf-8 -*-
-import os
-import biothings
+import logging
 from importlib import import_module
+from biothings.utils.es import get_es
+import json
+from os.path import abspath
 
 # Error class
 class BiothingConfigError(Exception):
     pass
 
-if 'config' in dir(biothings):
-    from biothings import config
-else:
-    # Import the module from environment variable
-    try:
-        config_module = os.environ['BIOTHING_CONFIG']
-    except:
-        raise BiothingConfigError("Make sure biothings is configure either by: setting BIOTHING_CONFIG environment variable with config module, or biothings.config_for_app is called before accessing settings.")
-    config = import_module(config_module)
-
-default = import_module('biothings.settings.default')
-
-class BiothingSettings(object):
-    config_vars = vars(config)
-    default_vars = vars(default)
-
-    def _return_var(self, key):
-        # return variable named key
+class BiothingWebSettings(object):
+    def __init__(self, config='biothings.www.settings.default'): 
+        self.config_mod = import_module(config)
         try:
-            return self.config_vars[key]
-        except KeyError:
-            return self.default_vars[key]
-
-    @property
-    def static_path(self):
-        return self._return_var('STATIC_PATH')
-
-    @property
-    def _annotation_endpoint(self):
-        return self._return_var('ANNOTATION_ENDPOINT')
-
-    @property
-    def _query_endpoint(self):
-        return self._return_var('QUERY_ENDPOINT')
-
-    @property
-    def _graph_query_endpoint(self):
-        return self._return_var('GRAPH_QUERY_ENDPOINT')
-
-    @property
-    def _api_version(self):
-        if self._return_var('API_VERSION'):
-            return self._return_var('API_VERSION')
-        else:
-            return ''
-
-    @property
-    def status_check_id(self):
-        return self._return_var('STATUS_CHECK_ID')
-
-    @property
-    def field_notes_path(self):
-        return self._return_var('FIELD_NOTES_PATH')
-
-    @property
-    def jsonld_context_path(self):
-        return self._return_var('JSONLD_CONTEXT_PATH')
-
-    @property
-    def nosetest_settings(self):
-        return self._return_var('NOSETEST_SETTINGS')
-
-    # *************************************************************************
-    # * Elasticsearch functions and properties
-    # *************************************************************************
-
-    @property
-    def es_query_module(self):
-        return self._return_var('ES_QUERY_MODULE')
-
-
-    @property
-    def es_host(self):
-        return self._return_var('ES_HOST')
-
-    @property
-    def es_index(self):
-        return self._return_var('ES_INDEX_NAME')
-
-    @property
-    def es_doc_type(self):
-        return self._return_var('ES_DOC_TYPE')
-
-    @property
-    def allowed_options(self):
-        return self._return_var('ALLOWED_OPTIONS')
-
-    @property
-    def scroll_time(self):
-        return self._return_var('ES_SCROLL_TIME')
-
-    @property
-    def scroll_size(self):
-        return self._return_var('ES_SCROLL_SIZE')
-
-    @property
-    def size_cap(self):
-        return self._return_var('ES_SIZE_CAP')
-
-    @property
-    def userquery_dir(self):
-        return self._return_var('USERQUERY_DIR')
+            with open(abspath(self.config_mod.JSONLD_CONTEXT_PATH), 'r') as json_file:
+                self._jsonld_context = json.load(json_file)
+        except:
+            self._jsonld_context = {}
+        # validate these settings?
+        self.validate()
     
-    # *************************************************************************
-    # * neo4j settings wrappers
-    # *************************************************************************
+    def __getattr__(self, name):
+        try:
+            return getattr(self.config_mod, name)
+        except AttributeError:
+            raise AttributeError("No setting named '{}' was found, check configuration module.".format(name))
+
+    def set_debug_level(self, debug=False):
+        ''' Are we debugging? '''
+        self._DEBUG = debug
+        return self
     
-    @property
-    def neo4j_query_module(self):
-        return self._return_var('NEO4J_QUERY_MODULE')
+    def generate_app_list(self):
+        ''' Generates the APP_LIST for tornado for this project, basically just adds the settings 
+            to kwargs in every handler's initialization. '''
+        return [(endpoint_regex, handler, {"web_settings": self}) for (endpoint_regex, handler) in self.APP_LIST]
 
-    @property
-    def is_neo4j_app(self):
-        iga = self._return_var('NEO4J_APP')
-        if (isinstance(iga, int) and iga == 1) or (isinstance(iga, str) and iga.lower() == 'true') or (isinstance(iga, bool) and iga):
-            return True
-        return False
+    def validate(self):
+        ''' validates this object '''
+        pass
 
-    @property
-    def neo4j_host(self):
-        return self._return_var('NEO4J_CYPHER_ENDPOINT')
+class BiothingESWebSettings(BiothingWebSettings):
+    ''' subclass with functions specific to elasticsearch backend '''
+    def __init__(self, config='biothings.www.settings.default'):
+        super(BiothingESWebSettings, self).__init__(config)
 
-    @property
-    def neo4j_username(self):
-        return self._return_var('NEO4J_USERNAME')
+        # get es client for web
+        self.es_client = self.get_es_client()
 
-    @property
-    def neo4j_password(self):
-        return self._return_var('NEO4J_PASSWORD')
-
-    # *************************************************************************
-    # * Google Analytics API tracking object functions
-    # *************************************************************************
-
-    @property
-    def private_module(self):
-        return self._return_var('PRIVATE_MODULE')
-
-    @property
-    def ga_event_for_get_action(self):
-        return self._return_var('GA_EVENT_GET_ACTION')
-
-    @property
-    def ga_event_for_post_action(self):
-        return self._return_var('GA_EVENT_POST_ACTION')
-
-    @property
-    def ga_event_category(self):
-        return self._return_var('GA_EVENT_CATEGORY')
-
-    @property
-    def ga_is_prod(self):
-        return self._return_var('GA_RUN_IN_PROD')
-
-    @property
-    def ga_account(self):
-        # Get from config file first
-        try:
-            return self.config_vars['GA_ACCOUNT']
-        except KeyError:
-            pass
-        
-        # Try to get from a private submodule on the server
-        try:
-            return vars(import_module(self.private_module))['ANALYTICS'][self.es_doc_type]
-        except (ImportError, KeyError):
-            pass
-        
-        # Fallback to default
-        return self.default_vars['GA_ACCOUNT']
-
-    @property
-    def ga_tracker_url(self):
-        return self._return_var('GA_TRACKER_URL')
-
-    # This function returns the object that is sent to google analytics for an API call
-    def ga_event_object(self, endpoint, action, data):
-        ret = {}
-        ret['category'] = self.ga_event_category
-        if action == 'GET':
-            ret['action'] = '_'.join([endpoint, self.ga_event_for_get_action])
-        elif action == 'POST':
-            ret['action'] = '_'.join([endpoint, self.ga_event_for_post_action])
-        else:
-            ret['action'] = action
-        if data:
-            ret['label'] = list(data.keys()).pop() 
-            ret['value'] = list(data.values()).pop()
-        return ret
+    def get_es_client(self):
+        ''' get the es client for this app '''
+        return get_es(self.ES_HOST)
--- biothings/www/settings/default.py
+++ biothings/www/settings/default.py
@@ -1,66 +1,177 @@
+from biothings.www.api.es.query import ESQuery as DefaultESQuery
+from biothings.www.api.es.query_builder import ESQueryBuilder as DefaultESQueryBuilder
+from biothings.www.api.es.transform import ESResultTransformer as DefaultESResultTransformer
+import re
+
 # *****************************************************************************
 # Elasticsearch variables
 # *****************************************************************************
 # elasticsearch server transport url
 ES_HOST = 'localhost:9200'
+
 # elasticsearch index name
-ES_INDEX_NAME = 'mybiothing_current'
+ES_INDEX = 'mybiothing_current'
 # elasticsearch document type
 ES_DOC_TYPE = 'biothing'
-# Only these options are passed to the elasticsearch query from kwargs
-ALLOWED_OPTIONS = ['_source', 'start', 'from_', 'size', 'sort', 'explain', 'version', 'aggs', 'fetch_all', 'fields']
+# Amount of time a scroll request is kept open
 ES_SCROLL_TIME = '1m'
+# Size of each scroll request return
 ES_SCROLL_SIZE = 1000
+# Maximum size of result return
 ES_SIZE_CAP = 1000
-ES_QUERY_MODULE = 'biothings.www.api.es'
+
 # For the userquery folder for this app
-USERQUERY_DIR = 'biothings.userqueries'
-# Graph defaults
-# By default turn graph app off
-NEO4J_APP = False
-# Default neo4j query module
-NEO4J_QUERY_MODULE = 'biothings.www.api.neo4j'
-# Address to query neo4j cypher endpoint
-NEO4J_CYPHER_ENDPOINT = 'http://localhost:7474/db/data/cypher'
-# Username/password for neo4j
-NEO4J_USERNAME = 'neo4j'
-NEO4J_PASSWORD = 'neo4j'
+USERQUERY_DIR = ''
 
 # default static path, relative to current working dir
 # (from where app is launched)
 STATIC_PATH = "static"
 
+# api version in the URL patterns and elsewhere
+API_VERSION = 'v1'
+# project URL routing
+APP_LIST = []
+
+# *****************************************************************************
+# Subclass of biothings.www.api.es.query_builder.ESQueryBuilder to build
+# queries for this app
+# *****************************************************************************
+ES_QUERY_BUILDER = DefaultESQueryBuilder
+# *****************************************************************************
+# Subclass of biothings.www.api.es.query.ESQuery to execute queries for this app
+# *****************************************************************************
+ES_QUERY = DefaultESQuery
+# *****************************************************************************
+# Subclass of biothings.www.api.es.transform.ESResultTransformer to transform
+# ES results for this app
+# *****************************************************************************
+ES_RESULT_TRANSFORMER = DefaultESResultTransformer
+
+OUTPUT_KEY_ALIASES = {'cadd':'schmadd', 'cadd/gene/ccds_id': 'cces_id'}
+
+# Global default cap for list inputs
+LIST_SIZE_CAP = 1000
+
+# TODO: Need to describe
+ANNOTATION_ID_REGEX_LIST = [(re.compile(r'rs[0-9]+', re.I), 'dbsnp.rsid')]
+
+# Keyword Argument Control
+# 
+# These parameters control which input kwargs go to which kwarg group for each
+# endpoint and operation (e.g. query GET, annotation POST, etc).
+# This allows explicit grouping of parameters into inputs of the pipeline
+# section that they are germane to.
+# CONTROL_KWARGS - general category, used for handler parameters (e.g. raw, rawquery)
+# ES_KWARGS go directly to the ESQuery function (e.g. fields, size...)
+# ESQB_KWARGS are used to instantiate a query builder class
+# TRANSFORM_KWARGS are used to instantiate a result transformer class
+# 
+# keys are parameter names, values are default values.  If value == None, no
+# default is inserted.
+
+# For annotation GET endpoint
+ANNOTATION_GET_CONTROL_KWARGS = {'raw': {'default': False, 'type': bool}, 
+                                 'rawquery': {'default': False, 'type': bool}}
+ANNOTATION_GET_ES_KWARGS = {'_source': {'default': None, 'type': list, 'max': 100, 'alias': ['fields', 'filter']}}
+ANNOTATION_GET_ESQB_KWARGS = {}
+ANNOTATION_GET_TRANSFORM_KWARGS = {'dotfield': {'default': False, 'type': bool}, 
+                                   'jsonld': {'default': False, 'type': bool},
+                                   '_sorted': {'default': True, 'type': bool}}
+
+# For annotation POST endpoint
+ANNOTATION_POST_CONTROL_KWARGS = {'raw': {'default': False, 'type': bool},
+                                  'rawquery': {'default': False, 'type': bool},
+                                  'ids': {'default': None, 'type': list, 'max': 1000}}
+ANNOTATION_POST_ES_KWARGS = {'_source': {'default': None, 'type': list, 'max': 100, 'alias': ['fields', 'filter']}}
+ANNOTATION_POST_ESQB_KWARGS = {}
+ANNOTATION_POST_TRANSFORM_KWARGS = {'dotfield': {'default': False, 'type': bool},
+                                    'jsonld': {'default': False, 'type': bool},
+                                    '_sorted': {'default': True, 'type': bool}}
+
+# For query GET endpoint
+QUERY_GET_CONTROL_KWARGS = {'raw': {'default': False, 'type': bool},
+                            'rawquery': {'default': False, 'type': bool},
+                            'q': {'default': None, 'type': str, 
+                                'translations': [
+                                    (re.compile(r'chr:', re.I), r'chrom:')
+                                ]
+                            },
+                            'scroll_id': {'default': None, 'type': str}}
+QUERY_GET_ES_KWARGS = {'_source': {'default': None, 'type': list, 'max': 100, 'alias': ['fields', 'filter']},
+                       'from': {'default': None, 'type': int, 'alias': 'skip'},
+                       'size': {'default': None, 'type': int, 'alias': 'limit'},
+                       'explain': {'default': None, 'type': bool},
+                       'aggs': {'default': None, 'type': list, 'max': 10, 'alias': 'facets'},
+                       'sort': {'default': None, 'type': list, 'max': 100}}
+QUERY_GET_ESQB_KWARGS = {'fetch_all': {'default': False, 'type': bool},
+                         'userquery': {'default': None, 'type': str},
+                         'q2': {'default': None, 'type': str},
+                         'q3': {'default': None, 'type': str},
+                         'q4': {'default': None, 'type': str},
+                         'q5': {'default': None, 'type': str}}
+QUERY_GET_TRANSFORM_KWARGS = {'dotfield': {'default': False, 'type': bool},
+                              'jsonld': {'default': False, 'type': bool},
+                              '_sorted': {'default': True, 'type': bool}}
+
+# For query POST endpoint
+QUERY_POST_CONTROL_KWARGS = {'q': {'default': None, 'type': list},
+                             'raw': {'default': False, 'type': bool},
+                             'rawquery': {'default': False, 'type': bool}}
+QUERY_POST_ES_KWARGS = {'_source': {'default': None, 'type': list, 'max': 100, 'alias': ['fields', 'filter']}}
+QUERY_POST_ESQB_KWARGS = {'scopes': {'default': None, 'type': list, 'max': 100, 
+                            'translations': [
+
+                            ]}}
+QUERY_POST_TRANSFORM_KWARGS = {'dotfield': {'default': False, 'type': bool}, 
+                               'jsonld': {'default': False, 'type': bool},
+                               '_sorted': {'default': True, 'type': bool}}
+
+# For metadata GET endpoint
+METADATA_GET_CONTROL_KWARGS = {}
+METADATA_GET_ES_KWARGS = {}
+METADATA_GET_ESQB_KWARGS = {}
+METADATA_GET_TRANSFORM_KWARGS = {'dev': {'default': False, 'type': bool}, 
+                                 'search': {'default': None, 'type': str},
+                                 'prefix': {'default': None, 'type': str}}
+
 # *****************************************************************************
 # Google Analytics Settings
 # *****************************************************************************
+
 # Google Analytics Account ID
-GA_ACCOUNT = 'google_account'
+GA_ACCOUNT = ''
+
 # Turn this to True to start google analytics tracking
 GA_RUN_IN_PROD = False
 
-# 'category' in google analytics event object
-GA_EVENT_CATEGORY = 'v1_api'
-# 'action' for get request in google analytics event object
-GA_EVENT_GET_ACTION = 'get'
-# 'action' for post request in google analytics event object
-GA_EVENT_POST_ACTION = 'post'
 # url for google analytics tracker
 GA_TRACKER_URL = 'mybiothing.info'
+GA_ACTION_QUERY_GET = 'query_get'
+GA_ACTION_QUERY_POST = 'query_post'
+GA_ACTION_ANNOTATION_GET = 'biothing_get'
+GA_ACTION_ANNOTATION_POST = 'biothing_post'
 
-# *****************************************************************************
-# URL settings
-# *****************************************************************************
-# For URL stuff
-ANNOTATION_ENDPOINT = 'biothing'
-QUERY_ENDPOINT = 'query'
-GRAPH_QUERY_ENDPOINT = 'graph_query'
-API_VERSION = 'v1'
-# TODO Fill in a status id here
-STATUS_CHECK_ID = 'status_id'
-# Path to a file containing a json object with information about elasticsearch fields
-FIELD_NOTES_PATH = 'field_notes_path'
-# For the path to the json-ld context
-JSONLD_CONTEXT_PATH = 'context'
-# Module that contains the nosetest config
-NOSETEST_SETTINGS = 'tests.nosetest_config'
-PRIVATE_MODULE = 'prmod'
+# parameter for JSONP
+JSONP_PARAMETER = 'callback'
+
+# Caching behavior
+# should caching be disabled by default on handlers?
+DISABLE_CACHING = False
+CACHE_MAX_AGE = 604800
+
+# Sentry project address
+SENTRY_CLIENT_KEY = ''
+
+# JSON-LD PATH
+JSONLD_CONTEXT_PATH = ''
+
+# Can turn msgpack functionality off here, will still load msgpack module if available, just won't
+# use it to compress requests
+ENABLE_MSGPACK = True
+
+ID_LIST_SPLIT_REGEX = re.compile('[\s\r\n+|,]+')
+
+DEFAULT_SCOPES = ['_id']
+
+# path to the git repository for the app-specific code, override
+APP_GIT_REPOSITORY = ''
