5ed5521f003dac27b2aeb93e1fddec1e12c308e7
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2017-01-01 07:56:56 -0800

    New ES specific query building functions go here

--- biothings/www/api/es/query_builder.py
+++ biothings/www/api/es/query_builder.py
@@ -1,86 +1,127 @@
-from biothings.www.logging import get_logger
+import logging
+import json
+from biothings.utils.common import is_seq, is_str
+from biothings.utils.userquery import get_userquery, get_userfilter
+try:
+    from re import fullmatch as match
+except ImportError:
+    from re import match
+
+class ESQueries(object):
+    def __init__(self, es_kwargs={}):
+        self.es_kwargs = es_kwargs
+
+    def _validate_json(self, d):
+        ''' dumps the dict to string and reads back in as json - should validate the json structure? '''
+        try:
+            return json.loads(json.dumps(d))
+        except:
+            raise ValueError('Could not validate query "{}"'.format(d))
+
+    def _es_query_template(self, query_type=None, query_kwargs=None, raw_query={}):
+        _ret = raw_query if raw_query else {"query":{query_type: query_kwargs}}
+        _ret.update(self.es_kwargs)
+        return self._validate_json(_ret)
+
+    def multi_match(self, query_kwargs):
+        return self._es_query_template(query_type="multi_match", query_kwargs=query_kwargs)
+
+    def match(self, query_kwargs):
+        return self._es_query_template(query_type="match", query_kwargs=query_kwargs)
+
+    def match_all(self, query_kwargs):
+        return self._es_query_template(query_type="match_all", query_kwargs=query_kwargs)
+
+    def query_string(self, query_kwargs):
+        return self._es_query_template(query_type="query_string", query_kwargs=query_kwargs)
+
+    def bool(self, query_kwargs):
+        return self._es_query_template(query_type="bool", query_kwargs=query_kwargs)
+
+    def user_query(self, raw_query):
+        return self._es_query_template(raw_query=raw_query)
 
 class ESQueryBuilder(object):
-    def __init__(self, options, regex_list=[], datasource_translation={}, logger_lvl=None):
+    def __init__(self, index, doc_type, options, es_options, scroll_options={}, 
+                       userquery_dir='', regex_list=[], default_scopes='_id'):
+        self.index = index
+        self.doc_type = doc_type
         self.options = options
+        self.es_options = es_options
+        self.scroll_options = scroll_options
         self.regex_list = regex_list
-        self.datasource_translation = datasource_translation
-        if logger_lvl:
-            self.logger = get_logger(mod_name=__name__, lvl=logger_lvl)
+        self.userquery_dir = userquery_dir
+        self.default_scopes = default_scopes
+        self.queries = ESQueries(es_options)
+
+    def _return_query_kwargs(self, query_kwargs):
+        _kwargs = {"index": self.index, "doc_type": self.doc_type}
+        _kwargs.update(query_kwargs)
+        return _kwargs 
+
+    def _get_term_scope(self, term):
+        _scopes = None
+        for (regex, scope) in self.regex_list:
+            if match(regex, term):
+                _scopes = scope
+                break
+        return _scopes
+    
+    def _build_single_query(self, term, scopes=None):
+        scopes = scopes or self.default_scopes
+        if len(scopes) == 1:
+            return self.queries.match({scopes[0]:{"query": "{}".format(term), "operator": "and"}})
         else:
-            self.logger = get_logger(mod_name=__name__)
+            return self.queries.multi_match({"query":"{}".format(term), "fields":scopes, "operator":"and"})
 
-    def annotation_POST_query(self, ids):
-        pass
-
-    def annotation_GET_query(self, id):
-        pass
-
-    def query_GET_query(self, q):
-        pass
-
-class ESQueryBuilder(object):
-    def __init__(self, **query_options):
-        self._query_options = query_options
-        self._options = self._query_options.pop('options', {})
-
-    def build_id_query(self, bid, scopes=None):
-        _default_scopes = '_id'
-        scopes = scopes or _default_scopes
-        if is_str(scopes):
-            _query = {
-                "match": {
-                    scopes: {
-                        "query": "{}".format(bid),
-                        "operator": "and"
-                    }
-                }
-            }
-        elif is_seq(scopes):
-            _query = {
-                "multi_match": {
-                    "query": "{}".format(bid),
-                    "fields": scopes,
-                    "operator": "and"
-                }
-            }
-        else:
-            raise ValueError('"scopes" cannot be "%s" type'.format(type(scopes)))
-        _q = {"query": _query}
-        self._query_options.pop("query", None)    # avoid "query" be overwritten by self.query_options
-        _q.update(self._query_options)
-        return _q
-
-    def build_multiple_id_query(self, bid_list, scopes=None):
-        """make a query body for msearch query."""
+    def _build_multiple_query(self, terms, scopes=None):
         _q = []
-        for id in bid_list:
-            _q.extend(['{}', json.dumps(self.build_id_query(id, scopes))])
-        _q.append('')
-        return '\n'.join(_q)
+        _infer_scope = True if not scopes else False
+        for term in terms:
+            if _infer_scope:
+                scopes = self._get_term_scope(term)
+            _q.extend(['{}', json.dumps(self._build_single_query(term, scopes=scopes))])
+        return self._return_query_kwargs({'body': '\n'.join(_q)})
 
-    def default_query(self, q):
-        return {
-            "query": {
-                "query_string": {
-                    "query": q.lstrip('*?')
-                }
-            }
-        }
+    def _default_query(self, q):
+        return self.queries.query_string({"query": q})
 
-    def user_query(self, q):
-        args = [q]
-        args.extend(self._options.userquery_args)
-        return json.loads(get_userquery(biothing_settings.userquery_dir, self._options.userquery).format(*args))
+    def _is_match_all(self, q):
+        return (q == '__all__')
+
+    def _is_user_query(self, q):
+        return self.options.userquery and self._user_query(q)
+
+    def _extra_query_types(self, q):
+        return {}
+
+    def _match_all(self, q):
+        return self.queries.match_all({})
+
+    def _scroll(self, scroll_id):
+        return {'body': {'scroll_id': scroll_id}, 'scroll': self.scroll_options.get('scroll', '1m')}
+
+    def _user_query(self, q):
+        _args = [q]
+        _args.extend([self.options.get(_key) for _key in ['q2', 'q3', 'q4', 'q5'] 
+                        if self.options.get(_key, None)])
+        _ret = json.loads(get_userquery(self.userquery_dir, self.options.userquery))
+        return self.queries.user_query(_ret)
+
+    def user_query_filter(self):
+        return json.loads(get_userfilter(self.userquery_dir, self.options.userquery))
+
+    def _get_query_filters(self):
+        _filter = []
+        if self.options.userquery and self.user_query_filter():
+            _filter.append(self.user_query_filter())
+        return _filter
 
     def get_query_filters(self):
-        '''Subclass to add specific filters'''
-        return []
+        ''' Override me '''
+        return self._get_query_filters()
 
     def add_query_filters(self, _query):
-        '''filters added here will be applied in a filtered query,
-           thus will affect the facet counts.
-        '''
         filters = self.get_query_filters()
         if not filters:
             return _query
@@ -96,74 +137,60 @@ class ESQueryBuilder(object):
 
         return _query
 
-    def generate_query(self, q):
-        '''
-        Return query dict according to passed arg "q". Can be:
-            - match query
-            - wildcard query
-            - raw_string query
-            - "match all" query
-        Also add query filters
-        '''
-        # Check if fielded/boolean query, excluding special goid query
-        # raw_string_query should be checked ahead of wildcard query, as
-        # raw_string may contain wildcard as well # e.g., a query
-        # "symbol:CDK?", should be treated as raw_string_query.
-        if self._is_user_query():
-            _query = self.user_query(q)
-        elif q == '__all__':
-            _query = {"match_all": {}}
-        elif self._is_raw_string_query(q):
-            #logging.debug("this is raw string query")
-            _query = self.raw_string_query(q)
-        elif self._is_wildcard_query(q):
-            #logging.debug("this is wildcard query")
-            _query = self.wildcard_query(q)
+    def _annotation_GET_query(self, bid):
+        _scopes = self._get_term_scope(bid)
+        if _scopes:
+            return self._return_query_kwargs({'body': self._build_single_query(bid, scopes=_scopes)})
         else:
-            #logging.debug("this is dis max query")
-            _query = self.dis_max_query(q)
+            # go to es.get
+            _get_kwargs = {'id': bid}
+            _get_kwargs.update(self.es_options)
+            return self._return_query_kwargs(_get_kwargs)
+    
+    def _annotation_POST_query(self, bids):
+        return self._build_multiple_query(terms=bids)
+
+    def _query_GET_query(self, q):
+        if self._is_user_query(q):
+            _query = self._user_query(q)
+        elif self._is_match_all(q):
+            _query = self._match_all(q)
+        else:
+            _query = self._extra_query_types(q)
+
+        if not _query:
+            _query = self._default_query(q)
 
         _query = self.add_query_filters(_query)
 
-        return _query
+        _ret = self._return_query_kwargs({'body': _query})
 
-    def _is_user_query(self):
-        ''' Return True if query is a userquery '''
-        if self._options.userquery:
-            return True
-        return False
+        if self.options.fetch_all:
+            _ret['body'].pop('sort', None)  # don't allow sorting for fetch all, defeats the purpose
+            _ret['body'].pop('size', None)
+            _ret.update(self.scroll_options)
+        return _ret
 
-    def _is_wildcard_query(self, query):
-        '''Return True if input query is a wildcard query.'''
-        return query.find('*') != -1 or query.find('?') != -1
+    def _query_POST_query(self, qs, scopes):
+        return self._build_multiple_query(terms=qs, scopes=scopes)
 
-    def _is_raw_string_query(self, query):
-        '''Return True if input query is a wildchar/fielded/boolean query.'''
-        for v in [':', '~', ' AND ', ' OR ', 'NOT ']:
-            if query.find(v) != -1:
-                return True
-        if query.startswith('"') and query.endswith('"'):
-            return True
-        return False
+    def _metadata_query(self):
+        return self._return_query_kwargs({})
 
-    def raw_string_query(self, q):
-        _query = {
-            "query_string": {
-                "query": "%(q)s",
-                # "analyzer": "string_lowercase",
-                "default_operator": "AND",
-                "auto_generate_phrase_queries": True
-            }
-        }
-        _query = json.dumps(_query)
-        try:
-            _query = json.loads(_query % {'q': q.replace('"', '\\"')})
-        except ValueError:
-            raise QueryError("invalid query term.")
-        return _query
+    def annotation_GET_query(self, bid):
+        return self._annotation_GET_query(bid)
+    
+    def annotation_POST_query(self, bids):
+        return self._annotation_POST_query(bids)
 
-    def wildcard_query(self, q):
-        raise NotImplemented("Wildcard queries not supported (or implement in subclass)")
+    def query_GET_query(self, q):
+        return self._query_GET_query(q)
 
-    def dis_max_query(self, q):
-        raise NotImplemented("Dis max queries not supported (or implement in subclass)")
+    def query_POST_query(self, qs, scopes):
+        return self._query_POST_query(qs, scopes)
+
+    def metadata_query(self):
+        return self._metadata_query()
+
+    def scroll(self, scroll_id):
+        return self._scroll(scroll_id)
