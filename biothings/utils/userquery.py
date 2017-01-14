0cec95e699856ad4541a6b54021cbba1538c1250
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2017-01-13 17:25:40 -0800

    Userquery checks existence of folders outside the scope of userquery.py

--- biothings/utils/userquery.py
+++ biothings/utils/userquery.py
@@ -10,15 +10,10 @@ def get_userquery(query_folder, query_name):
     except KeyError:
         pass
 
-    # get the query from file
+    # get the query from file, assumed that query_folder/query_name/query.txt exists
     query_dir = os.path.join(query_folder, query_name)
-    if (os.path.exists(query_dir) and os.path.isdir(query_dir) 
-        and os.path.exists(os.path.join(query_dir, 'query.txt'))):
-        with open(os.path.join(query_dir, 'query.txt'), 'r') as query_handle:
-            query_cache[query_name] = query_handle.read()
-    else:
-        return '{{}}'
-    
+    with open(os.path.join(query_dir, 'query.txt'), 'r') as query_handle:
+        query_cache[query_name] = query_handle.read()
     #logging.debug("query_cache[{}]: {}".format(query_name, query_cache[query_name]))
 
     return query_cache[query_name]
@@ -29,14 +24,10 @@ def get_userfilter(query_folder, query_name):
     except KeyError:
         pass
 
+    # get the filter from file, assumed that query_folder/query_name/filter.txt exists
     query_dir = os.path.join(query_folder, query_name)
-    if (os.path.exists(query_dir) and os.path.isdir(query_dir) 
-        and os.path.exists(os.path.join(query_dir, 'filter.txt'))):
-        with open(os.path.join(query_dir, 'filter.txt'), 'r') as filter_handle:
-            filter_cache[query_name] = filter_handle.read()
-    else:
-        return '{}'
-    
+    with open(os.path.join(query_dir, 'filter.txt'), 'r') as filter_handle:
+        filter_cache[query_name] = filter_handle.read()
     #logging.debug("filter_cache[{}]: {}".format(query_name, filter_cache[query_name]))
 
     return filter_cache[query_name]
