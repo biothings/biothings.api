7b17b253ced04444d8fbf735168ba95d864e1a07
cyrus0824 <cyrus0824@users.noreply.github.com>
2017-06-16 09:12:22 -0700

    Update tracking.py
    Making default actions/categories be NA for tracking data

--- biothings/utils/www/tracking.py
+++ biothings/utils/www/tracking.py
@@ -7,7 +7,7 @@ class StandaloneTrackingMixin:
     def self_track(self, data={}):
         no_tracking = self.get_argument('no_tracking', None)
         if not no_tracking and self.web_settings.STANDALONE_TRACKING_URL:
-            req = HTTPRequest(self.web_settings.STANDALONE_TRACKING_URL, method='POST', body='category={0}&action={1}'.format(data.get('category', ''), data.get('action', '')))
+            req = HTTPRequest(self.web_settings.STANDALONE_TRACKING_URL, method='POST', body='category={0}&action={1}'.format(data.get('category', 'NA'), data.get('action', 'NA')))
 
             #now send actual async requests
             http_client = AsyncHTTPClient()
