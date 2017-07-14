1f32c0d25a28c2d44f944cfcd9e31c8cb6ea9004
Chunlei Wu <anewgene@yahoo.com>
2017-07-14 16:47:28 -0700

    modified the intro section of index.rst.

--- docs/conf.py
+++ docs/conf.py
@@ -49,9 +49,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'biothings.api'
-copyright = u'2016, Su Lab'
-author = u'Su Lab'
+project = u'BioThings'
+copyright = u'2017, BioThings'
+author = u'BioThings team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -203,7 +203,7 @@ html_static_path = ['_static']
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'biothingsapidoc'
+htmlhelp_basename = 'biothings'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -225,8 +225,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'biothingsapi.tex', u'biothings.api Documentation',
-   u'Su Lab', 'manual'),
+  (master_doc, 'biothings.tex', u'BioThings SDK Documentation',
+   u'BioThings team', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -255,7 +255,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'biothingsapi', u'biothings.api Documentation',
+    (master_doc, 'biothings', u'BioThings SDK Documentation',
      [author], 1)
 ]
 
@@ -269,7 +269,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'biothingsapi', u'biothings.api Documentation',
+  (master_doc, 'biothings', u'BioThings SDK Documentation',
    author, 'biothingsapi', 'One line description of project.',
    'Miscellaneous'),
 ]
