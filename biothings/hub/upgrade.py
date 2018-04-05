cc281ddaede44729b6f2462472a73bdd6ead8dd4
Sebastien Lelong <slelong@scripps.edu>
2018-04-05 10:21:19 -0700

    data_plugin collection needs same upgrade as src_dump when migration to 0.2

--- biothings/hub/upgrade.py
+++ biothings/hub/upgrade.py
@@ -1,12 +1,12 @@
 import sys
 
-from biothings.utils.hub_db import get_src_dump, get_hub_db_conn, dump, restore
+from biothings.utils.hub_db import get_src_dump, get_data_plugin, get_hub_db_conn, dump, restore
 from biothings import config
 logging = config.logger
 
 def migrate_0dot1_to_0dot2():
     """
-    mongodb src_dump changed:
+    mongodb src_dump/data_plugin changed:
         1. "data_folder" and "release" under "download"
         2. "data_folder" and "release" in upload.jobs[subsrc] taken from "download"
         3. no more "err" under "upload"
@@ -14,51 +14,54 @@ def migrate_0dot1_to_0dot2():
         5. "pending_to_upload" is now "pending": ["upload"]
     """
     src_dump = get_src_dump()
-    srcs = [src for src in src_dump.find()]
-    wasdue = False
-    for src in srcs:
-        logging.info("Converting '%s'" % src["_id"])
-        # 1.
-        for field in ["data_folder","release"]:
-            if field in src:
-                logging.debug("%s: found '%s' in document, moving under 'download'" % (src["_id"],field))
-                try:
-                    src["download"][field] = src.pop(field)
-                    wasdue = True
-                except KeyError as e:
-                    logging.warning("%s: no such field '%s' found, skip it (error: %s)" % (src["_id"],field,e))
-        # 2.
-        for subsrc_name in src.get("upload",{}).get("jobs",{}):
+    data_plugin = get_data_plugin()
+    for srccol in [src_dump,data_plugin]:
+        logging.info("Converting collection %s" % srccol)
+        srcs = [src for src in srccol.find()]
+        wasdue = False
+        for src in srcs:
+            logging.info("\tConverting '%s'" % src["_id"])
+            # 1.
             for field in ["data_folder","release"]:
-                if not field in src["upload"]["jobs"][subsrc_name]:
-                    logging.debug("%s: no '%s' found in upload jobs, taking it from 'download' (or from root keys)" % (src["_id"],field))
+                if field in src:
+                    logging.debug("\t\t%s: found '%s' in document, moving under 'download'" % (src["_id"],field))
                     try:
-                        src["upload"]["jobs"][subsrc_name][field] = src["download"][field]
+                        src["download"][field] = src.pop(field)
                         wasdue = True
-                    except KeyError:
+                    except KeyError as e:
+                        logging.warning("\t\t%s: no such field '%s' found, skip it (error: %s)" % (src["_id"],field,e))
+            # 2.
+            for subsrc_name in src.get("upload",{}).get("jobs",{}):
+                for field in ["data_folder","release"]:
+                    if not field in src["upload"]["jobs"][subsrc_name]:
+                        logging.debug("\t\t%s: no '%s' found in upload jobs, taking it from 'download' (or from root keys)" % (src["_id"],field))
                         try:
-                            src["upload"]["jobs"][subsrc_name][field] = src[field]
+                            src["upload"]["jobs"][subsrc_name][field] = src["download"][field]
                             wasdue = True
                         except KeyError:
-                            logging.warning("%s: no such field '%s' found, skip it" % (src["_id"],field))
-        # 3. & 4.
-        for field in ["err","status"]:
-            if field in src.get("upload",{}):
-                logging.debug("%s: removing '%s' key from 'upload'" % (src["_id"],field))
-                src["upload"].pop(field)
+                            try:
+                                src["upload"]["jobs"][subsrc_name][field] = src[field]
+                                wasdue = True
+                            except KeyError:
+                                logging.warning("\t\t%s: no such field '%s' found, skip it" % (src["_id"],field))
+            # 3. & 4.
+            for field in ["err","status"]:
+                if field in src.get("upload",{}):
+                    logging.debug("\t\t%s: removing '%s' key from 'upload'" % (src["_id"],field))
+                    src["upload"].pop(field)
+                    wasdue = True
+            # 5.
+            if "pending_to_upload" in src:
+                logging.debug("\t%s: found 'pending_to_upload' field, moving to 'pending' list" % src["_id"])
+                src.pop("pending_to_upload")
                 wasdue = True
-        # 5.
-        if "pending_to_upload" in src:
-            logging.debug("%s: found 'pending_to_upload' field, moving to 'pending' list" % src["_id"])
-            src.pop("pending_to_upload")
-            wasdue = True
-            if not "upload" in src.get("pending",[]):
-                src.setdefault("pending",[]).append("upload")
-        if wasdue:
-            logging.info("Finishing converting document for '%s'" % src["_id"])
-            src_dump.save(src)
-        else:
-            logging.info("Document for '%s' already converted" % src["_id"])
+                if not "upload" in src.get("pending",[]):
+                    src.setdefault("pending",[]).append("upload")
+            if wasdue:
+                logging.info("\tFinishing converting document for '%s'" % src["_id"])
+                srccol.save(src)
+            else:
+                logging.info("\tDocument for '%s' already converted" % src["_id"])
 
 
 def migrate(from_version, to_version,restore_if_failure=True):
