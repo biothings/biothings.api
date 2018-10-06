a90a1343ad72d7184b51419630095900ef66a183
Sebastien Lelong <slelong@scripps.edu>
2018-10-06 08:55:19 -0700

    fix studio issue https://github.com/biothings/biothings_studio/issues/5 "delete a plugin when more than one plugin shows multiple modals"5 "delete a plugin when more than one plugin shows multiple modals"5 "delete a plugin when more than one plugin shows multiple modals"5 "delete a plugin when more than one plugin shows multiple modals"5 "delete a plugin when more than one plugin shows multiple modals"

--- biothings/hub/webapp/src/BaseDataSource.vue
+++ biothings/hub/webapp/src/BaseDataSource.vue
@@ -124,7 +124,7 @@ export default {
             })
         },
         unregister: function() {
-            $('.ui.basic.unregister.modal')
+            $(`.${this.source.name}.ui.basic.unregister.modal`)
             .modal("setting", {
                 onApprove: function () {
                     var url = $(this).find("input.plugin_url").val();
