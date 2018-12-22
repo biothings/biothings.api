d20761d60763caca1e380263c0094c9b9be230b6
Chunlei Wu <anewgene@yahoo.com>
2018-12-21 21:29:01 -0800

    added web_extra_requires and code cleanup

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 import os
 import glob
-from setuptools import setup, find_packages
 from subprocess import check_output
 from subprocess import CalledProcessError
+from setuptools import setup, find_packages
 
 setup_path = os.path.dirname(__file__)
 
+
 def read(fname):
-    return open(os.path.join(setup_path, fname),encoding="utf8").read()
+    return open(os.path.join(setup_path, fname), encoding="utf8").read()
+
 
 REPO_URL = "https://github.com/biothings/biothings.api"
+
 
 # get version
 version = __import__('biothings').get_version()
@@ -18,13 +21,13 @@ version = __import__('biothings').get_version()
 # version gets set to MAJOR.MINOR.# commits on master branch if installed from pip repo
 # otherwise to MAJOR.MINOR.MICRO as defined in biothings.version
 try:
-    num_commits = check_output("git rev-list --count master", shell=True).decode('utf-8').strip('\n')
-except:
+    num_commits = check_output("git rev-list --count master", shell=True).strip().decode('utf-8')
+except CalledProcessError:
     num_commits = ''
 
 # Calculate commit hash, should fail if installed from source or from pypi
 try:
-    commit_hash = check_output("git rev-parse HEAD", shell=True).decode('utf-8').strip('\n')
+    commit_hash = check_output("git rev-parse HEAD", shell=True).strip().decode('utf-8')
 except CalledProcessError:
     commit_hash = ''
 
@@ -33,30 +36,46 @@ if commit_hash or num_commits:
     with open('biothings/.git-info', 'w') as git_file:
         git_file.write("{}.git\n{}\n{}".format(REPO_URL, commit_hash, num_commits))
 
+
+# very minimal requirement for running biothings.web
 install_requires = [
     'requests>=2.8.1',
     'tornado>=5.1',
     'elasticsearch==6.1.1',
     'gitpython==2.1.11'
 ]
+
+# extra requirements for biothings.web
+web_extra_requires = [
+    'msgpack>=0.6.0',   # support format=msgpack
+    'PyYAML>=3.13'      # support format=yaml
+]
+
+# extra requirements to run biothings.hub
 hub_requires = [
     'beautifulsoup4',
     'aiocron',
-    'asyncssh==1.7.1', # needs libffi-dev installed (apt-get)
+    'asyncssh==1.7.1',  # needs libffi-dev installed (apt-get)
     'pymongo',
     'psutil',
     'jsonpointer',
     'IPython',
     'boto',
     'boto3',
-    'multiprocessing_on_dill', # can replace pickler in concurrent.futures
+    'multiprocessing_on_dill',  # can replace pickler in concurrent.futures
     'dill',
-    'pyinotify', # hub reloader
-    'prettytable', # diff report renderer
+    'pyinotify',        # hub reloader
+    'prettytable',      # diff report renderer
     'sockjs-tornado==1.0.6',
     'networkx>=2.1',
     'jsonschema>=2.6.0',
-    'pip', # auto-install requirements from plugins
+    'pip',              # auto-install requirements from plugins
+]
+
+# extra requirements for building docs
+docs_requires = [
+    'sphinx',
+    'sphinx_rtd_theme'
 ]
 
 setup(
@@ -91,7 +110,8 @@ setup(
     ],
     install_requires=install_requires,
     extras_require={
+        'web_extra': web_extra_requires,
         'hub': hub_requires,
-        'dev':  hub_requires + ['sphinx' + 'sphinx_rtd_theme']
+        'dev':  web_extra_requires + hub_requires + docs_requires
     },
 )
