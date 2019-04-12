ac1c7e29082825936c938c52817bf3039be004cd
Sebastien Lelong <slelong@scripps.edu>
2019-04-12 11:43:15 -0700

    - load skip compat flag at startup - fix tab order

--- biothings/hub/webapp/src/App.vue
+++ biothings/hub/webapp/src/App.vue
@@ -270,6 +270,7 @@
                 this.conn = JSON.parse(last);
             }
             this.setupConnection();
+            this.skip_studio_compat = Vue.localStorage.get("skip_studio_compat");
         },
         created () {
             console.log("App created");
@@ -705,8 +706,10 @@
             }
         },
         toggleCheckCompat(event) {
+            console.log("in toggleCheckCompat");
             var skip = $("#skip_compat").prop("checked");
             Vue.localStorage.set("skip_studio_compat",skip.toString());
+            this.skip_studio_compat = skip;
 
         }
     }
