8552ea3bd5c8c785affea838444c3fe26f95591e
Jerry <xzhou@scripps.edu>
2019-04-14 02:54:14 -0700

    Comply with test helper update

--- biothings/tests/tests.py
+++ biothings/tests/tests.py
@@ -16,9 +16,9 @@ from functools import wraps
 
 import requests
 
+from biothings.tests import BiothingsTestCase
 from biothings.tests.settings import BiothingTestSettings
-from biothings.tests.test_helper import (BiothingsTestCase,
-                                         parameterized, parameters)
+from biothings.tests.helper import parameterized, parameters
 
 try:
     import msgpack
@@ -39,22 +39,23 @@ except:
 
 PATTR = '%values'
 
+
 @parameterized
 class BiothingGenericTests(BiothingsTestCase):
-    __test__ = False # don't run nosetests on this class directly
+    __test__ = False  # don't run nosetests on this class directly
 
-    # Make these class variables so that tornadorequesthelper plays nice.    
+    # Make these class variables so that tornadorequesthelper plays nice.
     host = os.getenv(bts.HOST_ENVAR_NAME, '')
     if not host:
         host = bts.NOSETEST_DEFAULT_URL
     host = host.rstrip('/')
-    api = host + '/' + bts.API_VERSION
+    api = '/' + bts.API_VERSION
 
     # Setup/tear down class for unittest
     @classmethod
     def setUpClass(cls):
         # get host and urls, etc
-        sys.stderr.write("Running tests on {}\n".format(cls.api))
+        sys.stderr.write("Running tests on {}\n".format(cls.host))
         pass
 
     @classmethod
@@ -71,9 +72,9 @@ class BiothingGenericTests(BiothingsTestCase):
     #############################################################
     # Test functions                                            #
     #############################################################
-        
+
     def test_main(self):
-        self.get_ok(self.host)
+        self.request(self.host)
 
     #############################################################
     # Annotation endpoint GET tests                             #
@@ -81,81 +82,88 @@ class BiothingGenericTests(BiothingsTestCase):
 
     def test_annotation_object(self):
         ''' Test that annotation object contains expected fields. '''
-        url = self.api + '/' + bts.ANNOTATION_ENDPOINT + '/' + bts.ANNOTATION_OBJECT_ID
-        res = self.json_ok(self.get_ok(url))
+        url = bts.ANNOTATION_ENDPOINT + '/' + bts.ANNOTATION_OBJECT_ID
+        res = self.request(url).json()
         for attr in bts.ANNOTATION_OBJECT_EXPECTED_ATTRIBUTE_LIST:
-            self.assertIn(attr, res, 'Expected field "{}" in returned object of query: {}'.format(attr, url))
+            self.assertIn(
+                attr, res, 'Expected field "{}" in returned object of query: {}'.format(
+                    attr, url))
 
     def test_annotation_GET_empty(self):
         ''' Test that the annotation GET endpoint handles empty inputs correctly. '''
-        self.get_404(self.api + '/' + bts.ANNOTATION_ENDPOINT)
-        self.get_404(self.api + '/' + bts.ANNOTATION_ENDPOINT + '/')
+        self.request('' + bts.ANNOTATION_ENDPOINT, expect_status=404)
+        self.request('' + bts.ANNOTATION_ENDPOINT + '/', expect_status=404)
 
     def test_annotation_GET_unicode(self):
         ''' Test that the annotation GET endpoint handles unicode string inputs correctly. '''
-        self.get_404(self.api + '/' + bts.ANNOTATION_ENDPOINT + '/' + bts.UNICODE_TEST_STRING)
+        self.request('' + bts.ANNOTATION_ENDPOINT + '/' + bts.UNICODE_TEST_STRING, expect_status=404)
 
     @parameters(bts.ANNOTATION_GET_FIELDS)
     def test_annotation_GET_fields(self, bid):
         ''' Test that the fields parameter on the annotation GET endpoint works as expected. '''
-        url = self.api + '/' + bts.ANNOTATION_ENDPOINT + '/' + bid
+        url = bts.ANNOTATION_ENDPOINT + '/' + bid
         # Test setup correctly?
-        self.assertRegexpMatches(bid, r'fields=.+', 
+        self.assertRegexpMatches(
+            bid, r'fields=.+',
             'Test setup error, "fields" parameter not specified or empty in url: {}'.format(url))
         # get the url with all fields
-        total_url = self.api + '/' + bts.ANNOTATION_ENDPOINT + '/' + _q(bid.split('?')[0])
+        total_url = bts.ANNOTATION_ENDPOINT + '/' + _q(bid.split('?')[0])
         # get the user specified fields
         true_fields = [x.strip() for x in self.parse_url(url, 'fields').split(',')]
         # get the original and total request (with specified fields, and all fields, respectively)
-        res = self.json_ok(self.get_ok(url))
-        res_total = self.json_ok(self.get_ok(total_url))
+        res = self.request(url).json()
+        res_total = self.request(total_url).json()
         # check the fields
         self.check_fields(res, res_total, true_fields,
-            bts.CHECK_FIELDS_SUBSET_ADDITIONAL_FIELDS)
+                          bts.CHECK_FIELDS_SUBSET_ADDITIONAL_FIELDS)
 
     @parameters(bts.ANNOTATION_GET_FILTER)
     def test_annotation_GET_filter(self, bid):
         ''' Test that the filter parameter on the annotation GET endpoint works as expected. '''
-        url = self.api + '/' + bts.ANNOTATION_ENDPOINT + '/' + bid
+        url = bts.ANNOTATION_ENDPOINT + '/' + bid
         # Test setup correctly?
-        self.assertRegexpMatches(bid, r'filter=.+', 
+        self.assertRegexpMatches(
+            bid, r'filter=.+',
             'Test setup error, "filter" parameter not specified or empty in url: {}'.format(url))
         # get the url with all fields
-        total_url = self.api + '/' + bts.ANNOTATION_ENDPOINT + '/' + _q(bid.split('?')[0])
+        total_url = bts.ANNOTATION_ENDPOINT + '/' + _q(bid.split('?')[0])
         # get the user specified fields
         true_filter = [x.strip() for x in self.parse_url(url, 'filter').split(',')]
         # get the original and total request (with specified fields, and all fields, respectively)
-        res = self.json_ok(self.get_ok(url))
-        res_total = self.json_ok(self.get_ok(total_url))
+        res = self.request(url).json()
+        res_total = self.request(total_url).json()
         # check the fields
         self.check_fields(res, res_total, true_filter,
-            bts.CHECK_FIELDS_SUBSET_ADDITIONAL_FIELDS)
+                          bts.CHECK_FIELDS_SUBSET_ADDITIONAL_FIELDS)
 
     @parameters(bts.ANNOTATION_GET_JSONLD)
     def test_annotation_GET_jsonld(self, bid):
         ''' Test that the annotation GET endpoint correctly returns the JSON-LD context information. '''
-        url = self.api + '/' + bts.ANNOTATION_ENDPOINT + '/' + bid
-        self.assertRegexpMatches(bid, r'jsonld=((1)|([Tt][Rr][Uu][Ee]))',
+        url = bts.ANNOTATION_ENDPOINT + '/' + bid
+        self.assertRegexpMatches(
+            bid, r'jsonld=((1)|([Tt][Rr][Uu][Ee]))',
             'Test setup error, "jsonld" parameter not specified or false in url: {}'.format(url))
-        res = self.json_ok(self.get_ok(url))
+        res = self.request(url).json()
         self.check_jsonld(res, jsonld_context)
 
     @parameters(bts.ANNOTATION_GET_MSGPACK)
     def test_annotation_GET_msgpack(self, bid):
         ''' Test that the annotation GET endpoint correctly returns msgpack format. '''
-        url = self.api + '/' + bts.ANNOTATION_ENDPOINT + '/' + bid
+        url = bts.ANNOTATION_ENDPOINT + '/' + bid
         # Test must be set up correctly
-        self.assertRegexpMatches(bid, r'msgpack=((1)|([Tt][Rr][Uu][Ee]))',
+        self.assertRegexpMatches(
+            bid, r'msgpack=((1)|([Tt][Rr][Uu][Ee]))',
             'Test setup error, "msgpack" parameter not specified or false in url: {}\n'.format(url))
         # Get object without msgpack
         total_url = re.sub(r'[?&]msgpack=((1)|([Tt][Rr][Uu][Ee]))', '', url)
         # get the total and msgpack results
-        res_t = self.json_ok(self.get_ok(total_url))
-        res_msgpack = self.convert_msgpack(self.msgpack_ok(self.get_ok(url)))
+        res_t = self.request(total_url).json()
+        res_msgpack = self.convert_msgpack(self.msgpack_ok(self.request(url).content))
         # remove took from both (should be different)
         res_t.pop('took', None)
         res_msgpack.pop('took', None)
-        self.assertDictEqual(res_t, res_msgpack, "msgpacked results and non-msgpacked results differ for query: {}".format(url))    
+        self.assertDictEqual(
+            res_t, res_msgpack, "msgpacked results and non-msgpacked results differ for query: {}".format(url))
     """ 
     def test_annotation_GET(self):
         ''' Function to test GETs to the annotation endpoint. 
@@ -169,13 +177,13 @@ class BiothingGenericTests(BiothingsTestCase):
         # Check some ids to make sure the resulting _id matches
 
         for (test_number, bid) in enumerate(ns.annotation_GET):
-            base_url = self.api + '/' + ns.annotation_endpoint + '/' + bid
-            get_url = self.api + '/' + ns.annotation_endpoint + '/' + _q(bid.split('?')[0]) + '?' + '?'.join(bid.split('?')[1:])
+            base_url = ns.annotation_endpoint + '/' + bid
+            get_url = ns.annotation_endpoint + '/' + _q(bid.split('?')[0]) + '?' + '?'.join(bid.split('?')[1:])
             # if it specifies a callback function, make sure it works
             if self.parse_url(base_url, 'callback'):
                 res = self.extract_results_from_callback(get_url)
             else:
-                res = self.json_ok(self.get_ok(get_url))
+                res = self.request(get_url).json()
             # Check that the returned ID matches 
             eq_(res['_id'], bid.split('?')[0])
             # Is this a jsonld query?
@@ -188,8 +196,8 @@ class BiothingGenericTests(BiothingsTestCase):
                 elif 'filter' in bid:
                     true_fields = [x.strip() for x in self.parse_url(base_url, 'filter').split(',')]
                 # Next get the object with no fields specified
-                total_url = self.api + '/' + ns.annotation_endpoint + '/' + _q(bid.split('?')[0])
-                res_total = self.json_ok(self.get_ok(total_url))
+                total_url = ns.annotation_endpoint + '/' + _q(bid.split('?')[0])
+                res_total = self.request(total_url).json()
                 # Check the fields
                 self.check_fields(res, res_total, true_fields, ns.additional_fields_for_check_fields_subset)
             
@@ -197,17 +205,17 @@ class BiothingGenericTests(BiothingsTestCase):
             self._extra_annotation_GET(bid, res)
         
         # insert non ascii characters
-        self.get_404(self.api + '/' + ns.annotation_endpoint + '/' + '\xef\xbf\xbd\xef\xbf\xbd')
+        self.request('' + ns.annotation_endpoint + '/' + '\xef\xbf\xbd\xef\xbf\xbd', expect_status=404)
         
         # test msgpack on first ID
         self.check_msgpack(self.api + '/' + ns.annotation_endpoint + '/' + _q(ns.annotation_GET[0].split('?')[0]))
 
         # test unicode string handling
-        self.get_404(self.api + '/' + ns.annotation_endpoint + '/' + ns.unicode_test_string)
+        self.request('' + ns.annotation_endpoint + '/' + ns.unicode_test_string, expect_status=404)
 
         # test empties 
-        self.get_404(self.api + '/' + ns.annotation_endpoint)
-        self.get_404(self.api + '/' + ns.annotation_endpoint + '/')
+        self.request('' + ns.annotation_endpoint, expect_status=404)
+        self.request('' + ns.annotation_endpoint + '/', expect_status=404)
 
     
         def test_annotation_POST(self):
@@ -219,7 +227,7 @@ class BiothingGenericTests(BiothingsTestCase):
                 filters
                 jsonld
         '''
-        base_url = self.api + '/' + ns.annotation_endpoint
+        base_url = ns.annotation_endpoint
         for (test_number, ddict) in enumerate(ns.annotation_POST):
             res = self.json_ok(self.post_ok(base_url, ddict))
             returned_ids = [h['_id'] if '_id' in h else h['query'] for h in res]
@@ -239,7 +247,7 @@ class BiothingGenericTests(BiothingsTestCase):
                     elif 'filter' in ddict:
                         true_fields = [f.strip() for f in ddict.get('filter').split(',')]
                     total_url = base_url + '/' + _q(hit['_id'])
-                    res_total = self.json_ok(self.get_ok(total_url))
+                    res_total = self.request(total_url).json()
                     self.check_fields(hit, res_total, true_fields, ns.additional_fields_for_check_fields_subset)
 
                 self._extra_annotation_POST(ddict, hit)
@@ -269,17 +277,17 @@ class BiothingGenericTests(BiothingsTestCase):
         ''' 
         # Test some simple GETs to the query endpoint, first check some queries to make sure they return some hits
         for (test_number, q) in enumerate(ns.query_GET):
-            base_url = self.api + '/' + ns.query_endpoint + '?q=' + q
+            base_url = ns.query_endpoint + '?q=' + q
             # parse callback
             if self.parse_url(base_url, 'callback'):
                 res = self.extract_results_from_callback(base_url)
             elif self.check_boolean_url_option(base_url, 'fetch_all'):
                 # Is this a fetch all query?
                 # TODO:  make this less crappy.
-                sres = self.json_ok(self.get_ok(base_url))
+                sres = self.request(base_url).json()
                 assert '_scroll_id' in sres, "_scroll_id not found for fetch_all query: {}".format(q)
                 scroll_hits = int(sres['total']) if int(sres['total']) <= 1000 else 1000
-                res = self.json_ok(self.get_ok(self.api + '/' + ns.query_endpoint + '?scroll_id=' + sres['_scroll_id']))
+                res = self.request(self.api + '/' + ns.query_endpoint + '?scroll_id=' + sres['_scroll_id']).json()
                 assert 'hits' in res, "No hits found for query: {}\nScroll ID: {}".format(q, sres['_scroll_id'])
                 assert len(res['hits']) == scroll_hits, "Expected a scroll size of {}, got a scroll size of {}".format(scroll_hits, len(res['hits']))
             else:
@@ -313,27 +321,27 @@ class BiothingGenericTests(BiothingsTestCase):
                         true_fields = [x.strip() for x in self.parse_url(base_url, 'fields').split(',')]
                     elif 'filter' in bid:
                         true_fields = [x.strip() for x in self.parse_url(base_url, 'filter').split(',')]
-                    total_url = self.api + '/' + ns.annotation_endpoint + '/' + _q(hit['_id'])
+                    total_url = ns.annotation_endpoint + '/' + _q(hit['_id'])
                     res_total = {}
-                    res_total = self.json_ok(self.get_ok(total_url))
+                    res_total = self.request(total_url).json()
                     if res_total:
                         self.check_fields(hit, res_total, true_fields, ns.additional_fields_for_check_fields_subset)
             # extra tests
             self._extra_query_GET(q, res)
         
         # test non-ascii characters
-        res_f = self.json_ok(self.get_ok(self.api + '/' + ns.query_endpoint + '?q=' + '\xef\xbf\xbd\xef\xbf\xbd'))
+        res_f = self.request(self.api + '/' + ns.query_endpoint + '?q=' + '\xef\xbf\xbd\xef\xbf\xbd').json()
         assert res_f['hits'] == [], 'Query with non ASCII characters injected failed'
         
         # test msgpack on first query only
         self.check_msgpack(self.api + '/' + ns.query_endpoint + '?q=' + ns.query_GET[0])
 
         # test unicode insertion
-        res = self.json_ok(self.get_ok(self.api + '/' + ns.query_endpoint + '?q=' + ns.unicode_test_string))
+        res = self.request(self.api + '/' + ns.query_endpoint + '?q=' + ns.unicode_test_string).json()
         assert res['hits'] == [], "GET to query endpoint failed with unicode test string"
 
         # test empty/error
-        res = self.json_ok(self.get_ok(self.api + '/' + ns.query_endpoint), checkerror=False)
+        res = self.request(self.api + '/' + ns.query_endpoint), checkerror=False)
         assert 'error' in res, "GET to query endpoint failed with empty query"
     
     def test_query_POST(self):
@@ -342,9 +350,9 @@ class BiothingGenericTests(BiothingsTestCase):
 
         '''
         #query via post
-        base_url = self.api + '/' + ns.query_endpoint
+        base_url = ns.query_endpoint
         for (test_number, ddict) in enumerate(ns.query_POST):
-            res = self.json_ok(self.post_ok(base_url, ddict))
+            res = self.json_ok(self.post_ok(base_url, ddict).json()
             returned_queries = [h['query'] for h in res]
             assert set(returned_queries) == set([x.strip() for x in ddict['q'].split(',')]), "Set of returned queries doesn't match set of requested queries for annotation POST"
             # Check that the number of returned objects matches the number of inputs
@@ -361,8 +369,8 @@ class BiothingGenericTests(BiothingsTestCase):
                         true_fields = [f.strip() for f in ddict.get('fields').split(',')]
                     elif 'filter' in ddict:
                         true_fields = [f.strip() for f in ddict.get('filter').split(',')]
-                    total_url = self.api + '/' + ns.annotation_endpoint + '/' + _q(hit['_id'])
-                    res_total = self.json_ok(self.get_ok(total_url))
+                    total_url = ns.annotation_endpoint + '/' + _q(hit['_id'])
+                    res_total = self.request(total_url).json()
                     self.check_fields(hit, res_total, true_fields, ns.additional_fields_for_check_fields_subset)
 
                 self._extra_query_POST(ddict, hit)
@@ -381,23 +389,25 @@ class BiothingGenericTests(BiothingsTestCase):
     """
 
     def test_metadata(self):
-        self.get_ok(self.host + '/metadata')
-        self.get_ok(self.api + '/metadata')
+        self.request('/metadata')
+        self.request('metadata')
 
     def test_get_fields(self):
-        res = self.json_ok(self.get_ok(self.api + '/metadata/fields'))
+        res = self.request(self.api + '/metadata/fields').json()
         # Check to see if there are enough keys
-        self.assertGreater(len(res), bts.MINIMUM_NUMBER_OF_ACCEPTABLE_FIELDS, 'Too few fields returned in the /metadata/fields endpoint')
+        self.assertGreater(len(res), bts.MINIMUM_NUMBER_OF_ACCEPTABLE_FIELDS,
+                           'Too few fields returned in the /metadata/fields endpoint')
 
         for field in bts.TEST_FIELDS_GET_FIELDS_ENDPOINT:
-            self.assertIn(field, res, '"{}" expected in response from /metadata/fields, but not found'.format(field))
+            self.assertIn(
+                field, res, '"{}" expected in response from /metadata/fields, but not found'.format(field))
 
     def test_status_endpoint(self):
-        self.get_ok(self.host + '/status')
-        # (testing failing status would require actually loading tornado app from there 
+        self.request('/status')
+        # (testing failing status would require actually loading tornado app from there
         #  and deal with config params...)
 
     @classmethod
     def suite(cls):
         tests = unittest.defaultTestLoader.getTestCaseNames(cls)
-        return unittest.TestSuite(map(cls,tests))
+        return unittest.TestSuite(map(cls, tests))
