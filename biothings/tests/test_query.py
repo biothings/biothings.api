5bbd106aa49f13550ee93c173f7f8684e4fa1d4b
Jerry <xzhou@scripps.edu>
2019-04-25 18:15:20 -0700

    Change match all query syntax

--- biothings/tests/test_query.py
+++ biothings/tests/test_query.py
@@ -20,21 +20,21 @@ class QueryTests(BiothingsTestCase):
 
     def test_01(self):
         ''' KWARGS CTRL Format Json '''
-        self.query(q='*', size='1')
+        self.query(q='__all__', size='1')
 
     def test_02(self):
         ''' KWARGS CTRL Format Yaml '''
-        res = self.request('query?q=*&size=1&out_format=yaml').text
+        res = self.request('query?q=__all__&size=1&format=yaml').text
         assert res.startswith('max_score:')
 
     def test_03(self):
         ''' KWARGS CTRL Format Html '''
-        res = self.request('query?q=*&size=1&out_format=html').text
+        res = self.request('query?q=__all__&size=1&format=html').text
         assert '<html>' in res
 
     def test_04(self):
         ''' KWARGS CTRL Format Msgpack '''
-        res = self.request('query?q=*&size=1&out_format=msgpack').content
+        res = self.request('query?q=__all__&size=1&format=msgpack').content
         self.msgpack_ok(res)
 
 
