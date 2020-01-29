5ee984033289eef1168f0b6b736894f0bf7a5125
Jerry <xzhou@scripps.edu>
2020-01-28 18:14:42 -0800

    Improve documentation

--- biothings/web/settings/__init__.py
+++ biothings/web/settings/__init__.py
@@ -28,12 +28,16 @@ class BiothingConfigError(Exception):
 class BiothingWebSettings(object):
     '''
     A container for the settings that configure the web API.
+
+    * Environment variables can override settings of the same names.
+    * Default values are defined in biothings.web.settings.default.
+
     '''
 
     def __init__(self, config='biothings.web.settings.default'):
         '''
-        The ``config`` init parameter specifies a module that configures
-        this biothing.  For more information see `config module`_ documentation. '''
+        :param config: a module that configures this biothing or its fully qualified name.
+        '''
 
         self._user = config if isinstance(config, types.ModuleType) else import_module(config)
         self._default = import_module('biothings.web.settings.default')
@@ -74,11 +78,15 @@ class BiothingWebSettings(object):
         return handlers
 
     def get_git_repo_path(self):
-
+        '''
+        Return the path of the codebase if the specified folder in settings exists or `None`.
+        '''
         return self._git_repo_path
 
     def validate(self):
-
+        '''
+        Validate the settings defined for this web server.
+        '''
         for rule in self.APP_LIST:
             if len(rule) == 2:
                 pass
@@ -105,7 +113,13 @@ class KnownLiveSelecter(ConnectionSelector):
         return connections[0]
 
 class BiothingESWebSettings(BiothingWebSettings):
-    ''' `BiothingWebSettings`_ subclass with functions specific to an elasticsearch backend '''
+    '''
+    `BiothingWebSettings`_ subclass with functions specific to an elasticsearch backend.
+
+    * Use the known live ES connection if more than one is specified.
+    * Cache source metadata stored under the _meta field in es indices.
+
+    '''
 
     ES_VERSION = elasticsearch.__version__[0]
 
@@ -145,7 +159,9 @@ class BiothingESWebSettings(BiothingWebSettings):
         self.tracking_payload = []
 
     def validate(self):
-
+        '''
+        Additional ES settings to validate.
+        '''
         super().validate()
 
         assert isinstance(self.ES_INDEX, str)
@@ -156,13 +172,27 @@ class BiothingESWebSettings(BiothingWebSettings):
         self.ES_INDICES[self.ES_DOC_TYPE] = self.ES_INDEX
 
     def get_es_client(self):
+        '''
+        Return the default blocking elasticsearch client.
+        The connection is created upon first call.
+        '''
         return self._connections.get_connection('sync')
 
     def get_async_es_client(self):
+        '''
+        Return the async elasitcsearch client. API calls return awaitable objects.
+        The connection is created upon first call.
+        '''
         return self._connections.get_connection('async')
 
     def get_source_metadata(self, biothing_type='doc', latest=True):
+        '''
+        Get metadata defined in the ES index.
 
+        :param biothing_type: If multiple biothings are defined, specify which here.
+        :param latest: If set to `false`, return the cached copy. Otherwise retrieve latest.
+
+        '''
         cached = biothing_type in self._source_metadata
 
         if latest or not cached:
@@ -192,6 +222,9 @@ class BiothingESWebSettings(BiothingWebSettings):
         return self._source_metadata[biothing_type]
 
     def get_field_notes(self):
+        '''
+        Return the cached field notes associated with this instance.
+        '''
         return self._fields_notes
 
     ##### COMPATIBILITY METHODS #####
@@ -208,9 +241,7 @@ class BiothingESWebSettings(BiothingWebSettings):
         pass
 
     def doc_url(self, bid):
-        ''' Function to return a url on this biothing API to the biothing object specified by bid.'''
         return os.path.join(self.URL_BASE, self.API_VERSION, self.ES_DOC_TYPE, bid)
 
     def available_fields_notes(self):
-        ''' Caches the available fields notes for this biothing '''
         return self._fields_notes
