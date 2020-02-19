a0d7b95750a727b96c487f6d169e4d10ab8bb288
Sebastien Lelong <sebastien.lelong@gmail.com>
2020-02-19 18:53:14 +0000

    force gitdb version compatible with gitpython 2.1.11

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ if commit_hash or num_commits:
 install_requires = [
     'requests>=2.21.0',
     'tornado==5.1.1',
+    'gitdb2==2.0.4', # 3.x not compatible with gitpython 2.1.11
     'gitpython==2.1.11',
     'elasticsearch==6.3.1',
     'elasticsearch-dsl>=6.3.1, <7.0.0',
