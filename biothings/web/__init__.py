a8bd0b610c45c2cbd7d928c4e533d1a523c83299
Jerry <xzhou@scripps.edu>
2020-03-29 23:34:44 -0700

    Unify API starting script

--- biothings/web/__init__.py
+++ biothings/web/__init__.py
@@ -1,108 +1,110 @@
-import os
-import sys
+"""
+    Biothings Web API
+
+    - Support running behind a load balancer. Understand x-headers.
+    TODO
+"""
+import logging
+
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
-import tornado.escape
+
 from biothings.web.settings import BiothingESWebSettings
-from biothings.utils.common import is_str
-from tempfile import NamedTemporaryFile
-from string import Template
 
-def _is_module(m):
-    try:
-        import types
-        if type(m) == types.ModuleType:
-            return True
-        import importlib
-        _ret = importlib.import_module(m)
-    except ImportError:
-        return False
+try:
+    from raven.contrib.tornado import AsyncSentryClient
+except ImportError:
+    __SENTRY_INSTALLED__ = False
+else:
+    __SENTRY_INSTALLED__ = True
 
-    return _ret
+class BiothingsAPI():
+    """
+    Configure a Biothings Web API.
 
-def _is_file(f):
-    return os.path.exists(os.path.abspath(f)) and os.path.isfile(os.path.abspath(f))
+    There are three parts to it:
+    * A biothings config module that defines the API.
+    * Tornado settings that control web behaviors.
+    * An asyncio event loop to run tornado.
 
-class BiothingsAPIApp(object):
-    def __init__(self, *args, **kwargs):
-        if kwargs.get('object_name', False) or (len(args) >= 1 and not _is_module(args[0]) and not _is_file(args[0])):
-            _arg = args[0] if args else ''
-            self._configure_by_object_name(object_name=kwargs.get('object_name', _arg))
-        elif ((kwargs.get('config_file', False) and _file_exists(kwargs['config_file'])) or
-            (len(args) >= 1 and _is_file(args[0]))):
-            _arg = args[0] if args else ''
-            self._configure_by_file(config_file=os.path.abspath(kwargs.get('config_file', _arg)))
-        elif ((kwargs.get('config_module', False) and _is_module(kwargs['config_module'])) or 
-            (len(args) >= 1 and _is_module(args[0]))):
-            _arg = args[0] if args else ''
-            self._configure_by_module(config_module=kwargs.get('config_module', _arg))
+    The API can be started with:
+    * An external event loop by calling get_server()
+    * A default tornado event loop by calling start()
+    """
+
+    def __init__(self, config=None, **settings):
+        self.config = config
+        self.settings = dict(autoreload=False)
+        self.settings.update(settings)
+        self.host = '127.0.0.1'
+
+    @staticmethod
+    def use_curl():
+        """
+        Use curl implementation for tornado http clients.
+        More on https://www.tornadoweb.org/en/stable/httpclient.html
+        """
+        tornado.httpclient.AsyncHTTPClient.configure(
+            "tornado.curl_httpclient.CurlAsyncHTTPClient")
+
+    def use_sentry(self, sentry_client_key):
+        """
+        Setup error monitoring with Sentry. More on:
+        https://docs.sentry.io/clients/python/integrations/#tornado
+        """
+        if __SENTRY_INSTALLED__:
+            self.settings['sentry_client'] = \
+                AsyncSentryClient(sentry_client_key)
+
+    def update(self, **settings):
+        """
+        Update Tornado application settings. More on:
+        https://www.tornadoweb.org/en/stable/web.html \
+        #tornado.web.Application.settings
+        """
+        self.settings.update(settings)
+
+    def debug(self, debug=True):
+        """
+        Configure API to run in debug mode.
+        * listen on all interfaces.
+        * log debug level message.
+        * enable autoreload etc.
+        """
+        # About debug mode in tornado:
+        # https://www.tornadoweb.org/en/stable/guide/running.html \
+        # #debug-mode-and-automatic-reloading
+        if debug:
+            self.host = '0.0.0.0'
+            self.settings.update({"debug": True})
+            logging.getLogger().setLevel(logging.DEBUG)
         else:
-            self._configure_by_kwargs(**kwargs)
+            self.host = '127.0.0.1'
+            self.settings.update({"debug": False})
+            logging.getLogger().setLevel(logging.WARNING)
 
-    def get_server(self,config_mod, **app_settings):
-        settings = BiothingESWebSettings(config=config_mod) 
-        app = tornado.web.Application(settings.generate_app_list(), **app_settings)
-        server = tornado.httpserver.HTTPServer(app)
+    def get_server(self, config=None, **settings):
+        """
+        Run API in an external event loop.
+        """
+        api = BiothingESWebSettings(config or self.config)
+        app = tornado.web.Application(api.generate_app_list(), **settings)
+        server = tornado.httpserver.HTTPServer(app, xheaders=True)
         return server
 
-    def start(self, debug=True, port=8000, address='127.0.0.1', app_settings={}):
-        if debug:
-            #import tornado.autoreload
-            import logging
-            logging.getLogger().setLevel(logging.DEBUG)
-            address='0.0.0.0'
+    def start(self, port=8000):
+        """
+        Run API in the default event loop.
+        """
+        http_server = self.get_server(**self.settings)
+        http_server.listen(port, self.host)
 
-        with NamedTemporaryFile(mode='w', suffix='.py', dir=os.path.abspath('.')) as _tempfile:
-            if not getattr(self, 'settings_mod', False):
-                if not getattr(self, 'settings_str', False):
-                    raise("Could not configure biothings app")
-                _tempfile.file.write(self.settings_str)
-                _tempfile.file.flush()
-                self.settings_mod = os.path.split(_tempfile.name)[1].split('.')[0]
-            self.settings = BiothingESWebSettings(config=self.settings_mod)
-            application = tornado.web.Application(self.settings.generate_app_list(), **app_settings)
-            http_server = tornado.httpserver.HTTPServer(application)
-            http_server.listen(port, address)
-            loop = tornado.ioloop.IOLoop.instance()
-            if debug:
-                #tornado.autoreload.start(loop)
-                logging.info('Server is running on "%s:%s"...' % (address, port))
-            loop.start() 
+        logging.debug('Server is running on "%s:%s"...',
+                      self.host, port)
 
-    def _configure_by_object_name(self, object_name):
-        # get the config file template
-        config_string = """from biothings.web.settings.default import *\n""" + \
-                        """from biothings.web.api.es.handlers import *\n""" + \
-                        """ES_INDEX = '${src_package}_current'\n""" + \
-                        """ES_DOC_TYPE = '${es_doctype}'\n""" + \
-                        """API_VERSION = 'v1'\n""" + \
-                        """APP_LIST = [(r'/status', StatusHandler), (r'/metadata/?', MetadataHandler), (r'/metadata/fields/?', MetadataHandler), (r'/{}/${annotation_endpoint}/(.+)/?'.format(API_VERSION), BiothingHandler), (r'/{}/${annotation_endpoint}/?$$'.format(API_VERSION), BiothingHandler), (r'/{}/query/?'.format(API_VERSION), QueryHandler), (r'/{}/metadata/?'.format(API_VERSION), MetadataHandler), (r'/{}/metadata/fields/?'.format(API_VERSION), MetadataHandler),]\n""" + \
-                        """GA_RUN_IN_PROD = False"""
-        settings_dict = {'src_package': 'my' + object_name.lower(),
-                         'es_doctype': object_name.lower(),
-                         'annotation_endpoint': object_name.lower()}
-       
-        self.settings_str = Template(config_string).substitute(settings_dict)            
+        loop = tornado.ioloop.IOLoop.instance()
+        loop.start()
 
-    def _configure_by_module(self, config_module):
-        self.settings_mod = config_module
 
-    def _configure_by_kwargs(self, **kwargs):
-        self.settings_str = """from biothings.web.settings.default import *\nfrom biothings.web.api.es.handlers import *\n"""
-        for (k,v) in kwargs.items():
-            if k == 'APP_LIST':
-                self.settings_str += '{k}=['.format(k=k)
-                for (reg, handler_str) in v:
-                    self.settings_str += "(r'{reg}', {handler}),".format(reg=reg, handler=handler_str)
-                self.settings_str += ']\n'
-            elif k in ['ES_QUERY_BUILDER', 'ES_QUERY', 'ES_RESULT_TRANSFORMER']:
-                self.settings_str += '{k}={v}\n'
-            elif is_str(v):
-                self.settings_str += '{k}="{v}"\n'.format(k=k, v=v)
-            else:
-                self.settings_str += '{k}={v}\n'
-        
-    def _configure_by_file(self, config_file):
-        with open(config_file, 'r') as config_handle:
-            self.settings_str = config_handle.read()
+BiothingsAPIApp = BiothingsAPI
--- biothings/web/index_base.py
+++ biothings/web/index_base.py
@@ -1,87 +1,63 @@
-'''BioThings API ioloop start utilities.
+"""
+    A Biothings API demo implementation that supports command line arguments.
 
-This module contains functions to configure and start the `base event loop <http://www.tornadoweb.org/en/stable/ioloop.html>`_ from command line args.  Command line processing is done using tornado.options, with the following arguments defined:
+    Command line arguments are processed by tornado.options
+    The following arguments are defined:
 
     * ``port``: the port to start the API on, **default** 8000
     * ``address``: the address to start the API on, **default** 127.0.0.1
     * ``debug``: start the API in debug mode, **default** False
-    * ``appdir``: path to API configuration directory, **default**: current working directory
+    * ``dir``: path to app directory. **default**: current working directory
 
-    The **main** function is the boot script for all BioThings API webservers.
-'''
-import sys
+"""
 import os
-import tornado.httpserver
-import tornado.ioloop
-import tornado.web
-import tornado.escape
-from tornado.options import define, options
+import sys
 
-__USE_SENTRY__ = True
-try:
-    from raven.contrib.tornado import AsyncSentryClient
-except ImportError:
-    __USE_SENTRY__ = False
+from tornado.options import define, options, Error
 
-    def AsyncSentryClient(*args, **kwargs):
-        pass
+from biothings.web import BiothingsAPI
 
 __USE_WSGI__ = False
 
-define("port", default=8000, help="run on the given port", type=int)
-define("address", default="127.0.0.1", help="run on localhost")
-define("debug", default=False, type=bool, help="run in debug mode")
-define("appdir", default=os.getcwd(), type=str,
-       help="path to app directory containing (at minimum) a config module")
+define("port", default=8000, type=int, help="run on the given port")
+define("address", default="127.0.0.1", help="host address to listen to")
+define("debug", default=False, type=bool, help="debug settings like detailed logging")
+define("dir", default=os.getcwd(), type=str, help="path to app directory and config")
 
 try:
     options.parse_command_line()
-except BaseException:
+except Error:
     pass
+else:
+    APP_PATH = os.path.abspath(options.dir)
+    if APP_PATH not in sys.path:
+        sys.path.append(APP_PATH)
 
-# assume config file is root of appdir
-src_path = os.path.abspath(options.appdir)
-if src_path not in sys.path:
-    sys.path.append(src_path)
 
-if options.debug:
-    import tornado.autoreload
-    import logging
-    logging.getLogger().setLevel(logging.DEBUG)
-    options.address = '0.0.0.0'
+def main(biothings_config='config', app_settings=None, sentry_client_key=None, use_curl=False):
+    """ Start a Biothings API Server
 
-def get_app(APP_LIST, **settings):
-    ''' Return an Application instance. '''
-    return tornado.web.Application(APP_LIST, **settings)
+        :param biothings_config: the biothings web config module to use
+        :param app_settings: `Tornado application settings
+        <http://www.tornadoweb.org/en/stable/web.html#tornado.web.Application.settings>`_
+        :param sentry_client_key: Application-specific key for attaching Sentry error monitor
+        :param use_curl: Overide the default simple_httpclient with curl_httpclient
+        <https://www.tornadoweb.org/en/stable/httpclient.html>
+    """
+    app_settings = app_settings or {}
+    api = BiothingsAPI(biothings_config)
 
-def main(APP_LIST, app_settings={}, debug_settings={}, sentry_client_key=None, use_curl=False):
-    ''' Main ioloop configuration and start
-
-        :param APP_LIST: a list of `URLSpec objects or (regex, handler_class) tuples <http://www.tornadoweb.org/en/stable/web.html#tornado.web.Application>`_
-        :param app_settings: `Tornado application settings <http://www.tornadoweb.org/en/stable/web.html#tornado.web.Application.settings>`_
-        :param debug_settings: Additional application settings for API debug mode
-        :param sentry_client_key: Application-specific key for attaching Sentry monitor to the application
-        :param use_curl: Overide the default simple_httpclient with curl_httpclient (Useful for Github Login) <https://www.tornadoweb.org/en/stable/httpclient.html>
-    '''
-    settings = app_settings
-    if options.debug:
-        settings.update(debug_settings)
-        settings.update({"debug": True})
-    application = get_app(APP_LIST, **settings)
-    if __USE_SENTRY__ and sentry_client_key:
-        application.sentry_client = AsyncSentryClient(sentry_client_key)
+    if app_settings:
+        api.settings.update(app_settings)
+    if sentry_client_key:
+        api.use_sentry(sentry_client_key)
     if use_curl:
-        tornado.httpclient.AsyncHTTPClient.configure(
-            "tornado.curl_httpclient.CurlAsyncHTTPClient")
-    http_server = tornado.httpserver.HTTPServer(application, xheaders=True)
-    http_server.listen(options.port, address=options.address)
-    loop = tornado.ioloop.IOLoop.instance()
-    if options.debug:
-        tornado.autoreload.start(loop)
-        logging.info('Server is running on "%s:%s"...' % (options.address, options.port))
-    loop.start()
+        api.use_curl()
+
+    api.debug(options.debug)
+    api.host = options.address
+    api.start(options.port)
 
 
 if __name__ == '__main__':
-    from biothings.web.settings import BiothingESWebSettings
-    main(BiothingESWebSettings().generate_app_list())
+    main()
