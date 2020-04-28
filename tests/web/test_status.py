0f16fe34654560f8889ad1f5b199cb8bfa2b3846
Jerry <xzhou@scripps.edu>
2020-04-27 19:50:55 -0700

    Update test case for status handler accordingly

--- tests/web/test_status.py
+++ tests/web/test_status.py
@@ -13,9 +13,28 @@ from setup import setup_es  # pylint: disable=unused-import
 class TestStatus(BiothingsTestCase):
 
     def test_01_get(self):
-
-        res = self.request('/status').text
-        assert res == 'OK'
+        """
+        {
+            "code": 200,
+            "status": "yellow",
+            "payload": {
+                "id": "1017",
+                "index": "bts_test",
+                "doc_type": "_all"
+            },
+            "response": {
+                "_index": "bts_test",
+                "_type": "gene",
+                "_id": "1017",
+                "_version": 1,
+                "found": true,
+                "_source": { ... }
+            }
+        }
+        """
+        res = self.request('/status').json()
+        assert res['code'] == 200
+        assert res['response']['found']
 
     def test_02_head(self):
 
