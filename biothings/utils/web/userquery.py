4a5c47a801affeee9b7a9771c9cc905547964730
Jerry <xzhou@scripps.edu>
2020-04-29 17:08:44 -0700

    Move userquery to utils

--- biothings/utils/web/userquery.py
+++ biothings/utils/web/userquery.py
@@ -1,35 +1,74 @@
-import os.path
-import re
-#import logging
 
-query_cache = {}
-filter_cache = {}
+import json
+import logging
+import os
+from copy import deepcopy
 
-def get_userquery(query_folder, query_name):
-    try:
-        return query_cache[query_name]
-    except KeyError:
-        pass
+from elasticsearch_dsl import Q
 
-    # get the query from file, assumed that query_folder/query_name/query.txt exists
-    query_dir = os.path.join(query_folder, query_name)
-    with open(os.path.join(query_dir, 'query.txt'), 'r') as query_handle:
-        query_cache[query_name] = re.sub(r'\{\{\{\{(?P<var>.*?)\}\}\}\}', '{\g<var>}', 
-                                  re.sub(r"\{", "{{", re.sub(r"\}", "}}", query_handle.read())))
-    #logging.debug("query_cache[{}]: {}".format(query_name, query_cache[query_name]))
 
-    return query_cache[query_name]
+class ESUserQuery():
 
-def get_userfilter(query_folder, query_name):
-    try:
-        return filter_cache[query_name]
-    except KeyError:
-        pass
+    def __init__(self, path):
 
-    # get the filter from file, assumed that query_folder/query_name/filter.txt exists
-    query_dir = os.path.join(query_folder, query_name)
-    with open(os.path.join(query_dir, 'filter.txt'), 'r') as filter_handle:
-        filter_cache[query_name] = filter_handle.read()
-    #logging.debug("filter_cache[{}]: {}".format(query_name, filter_cache[query_name]))
+        self._queries = {}
+        self._filters = {}
+        try:
+            for (dirpath, dirnames, filenames) in os.walk(path):
+                if dirnames:
+                    self.logger.info("Detected user query folders: %s.", dirnames)
+                    continue
+                for filename in filenames:
+                    with open(os.path.join(dirpath, filename)) as text_file:
+                        if 'query' in filename:
+                            ## alternative implementation
+                            # self._queries[os.path.basename(dirpath)] = text_file.read()
+                            ##
+                            self._queries[os.path.basename(dirpath)] = json.load(text_file)
+                        elif 'filter' in filename:
+                            self._filters[os.path.basename(dirpath)] = json.load(text_file)
+        except Exception:
+            self.logger.exception('Error loading user queries.')
 
-    return filter_cache[query_name]
+    def has_query(self, named_query):
+
+        return named_query in self._queries
+
+    def has_filter(self, named_query):
+
+        return named_query in self._filters
+
+    def get_query(self, named_query, **kwargs):
+
+        def in_place_sub(dic, kwargs):
+            for key in dic:
+                if isinstance(dic[key], dict):
+                    in_place_sub(dic[key], kwargs)
+                elif isinstance(dic[key], list):
+                    for item in dic[key]:
+                        in_place_sub(item, kwargs)
+                elif isinstance(dic[key], str):
+                    dic[key] = dic[key].format(**kwargs).format(**kwargs)  # {{q}}
+
+        dic = deepcopy(self._queries.get(named_query))
+        in_place_sub(dic, kwargs)
+        key, val = next(iter(dic.items()))
+        return Q(key, **val)
+
+        ## alternative implementation
+        # string = self._queries.get(named_query)
+        # string1 = re.sub(r"\}", "}}", string)
+        # string2 = re.sub(r"\{", "{{", string1)
+        # string3 = re.sub(r'\{\{\{\{(?P<var>.*?)\}\}\}\}', r'{\g<var>}', string2)
+        # return string3
+        ##
+
+    def get_filter(self, named_query):
+
+        dic = self._filters.get(named_query)
+        key, val = next(iter(dic.items()))
+        return Q(key, **val)
+
+    @property
+    def logger(self):
+        return logging.getLogger(__name__)
