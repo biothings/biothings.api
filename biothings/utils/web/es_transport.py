771d36b3e7e908be066a37cec838b074b684f52d
Jerry <xzhou@scripps.edu>
2020-05-13 14:12:33 -0700

    Sniff should fail quickly

--- biothings/utils/web/es_transport.py
+++ biothings/utils/web/es_transport.py
@@ -51,8 +51,8 @@ class BiothingsAsyncTransport(AsyncTransport):
             session = aiohttp.ClientSession(timeout=timeout)
             try:
                 url = f"http://{host['host']}:{host['port']}"
-                yield from session.head(url, raise_for_status=True)
-                # yield from session.head(url, raise_for_status=True, timeout=1.5)
+                # yield from session.head(url, raise_for_status=True) TODO
+                yield from session.head(url, raise_for_status=True, timeout=1.5)
                 valid_hosts.append(host)
             except aiohttp.ClientError as exc:
                 logger.warning(str(exc))
