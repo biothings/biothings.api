9d7c9797e27a1345430c9be4ce101aa346ea1dc9
Jerry <xzhou@scripps.edu>
2020-06-15 16:14:18 -0700

    Add metadata fields second level entry

--- biothings/web/settings/default.py
+++ biothings/web/settings/default.py
@@ -46,6 +46,7 @@ APP_LIST = [
     (r"/", 'biothings.web.handlers.FrontPageHandler'),
     (r"/({pre})/", 'tornado.web.RedirectHandler', {"url": "/{0}"}),
     (r"/{pre}/status", 'biothings.web.handlers.StatusHandler'),
+    (r"/{pre}/metadata/fields/?", 'biothings.web.handlers.MetadataFieldHandler'),
     (r"/{pre}/metadata/?", 'biothings.web.handlers.MetadataSourceHandler'),
     (r"/{pre}/{ver}/spec/?", 'biothings.web.handlers.APISpecificationHandler'),
     (r"/{pre}/{ver}/{typ}/metadata/fields/?", 'biothings.web.handlers.MetadataFieldHandler'),
