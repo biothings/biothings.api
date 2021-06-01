a0fa3705a399b0074341e228754b83adc8fc6390
Jerry Zhou <xzhou@scripps.edu>
2021-06-01 14:22:22 -0700

    Update indexer.py

--- biothings/hub/dataindex/indexer.py
+++ biothings/hub/dataindex/indexer.py
@@ -477,7 +477,7 @@ class Indexer(object):
         return {
             # as of ES6, include_in_all was removed, we need to create our own "all" field
             "query": {
-                "default_field": "all"
+                "default_field": "_id,all"
             },
             "codec": "best_compression",
             # as of ES6, analysers/tokenizers must be defined in index settings, during creation
