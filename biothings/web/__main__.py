fbeda46ab84e24c155558c310d0b1e281146d402
Jerry <xzhou@scripps.edu>
2021-07-31 18:37:47 -0700

    support running biothings.web as a module

---
+++ biothings/web/__main__.py
@@ -0,0 +1,17 @@
+"""
+    Biothings API
+    
+    Support running biothings.web as a module
+
+    >> python -m biothings.web
+    >> python -m biothings.web --dir=~/mygene.info/src
+    >> python -m biothings.web --dir=~/mygene.info/src --conf=config_web
+    >> python -m biothings.web --conf=biothings.web.settings.default
+
+    See more supported parameters in biothings.web.launcher.
+
+"""
+
+from biothings.web.launcher import main
+
+main()
