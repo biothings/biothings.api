19fe1cbefbaf61e0e4745d2597faa6d28e729dcf
Chunlei Wu <anewgene@yahoo.com>
2022-07-09 10:16:09 -0700

    build: :arrow_up: updated asyncssh version
    removed a deprecation warning in Python 3.10

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ hub_requires = [
     'beautifulsoup4',   # used in dumper.GoogleDriveDumper
     'aiocron==1.8',     # setup scheduled jobs
     'aiohttp==3.8.1',   # elasticsearch requires aiohttp>=3,<4
-    'asyncssh==2.5.0',  # needs libffi-dev installed (apt-get)
+    'asyncssh==2.11.0',  # needs libffi-dev installed (apt-get)
     'pymongo>=4.1.0,<5.0',  # support MongoDB 5.0 since v3.12.0
     'psutil',
     'jsonpointer',      # for utils.jsonpatch
