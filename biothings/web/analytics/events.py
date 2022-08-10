f9171a0bf514d526a0090e66754c2cc02af71a1d
remoteeng00 <remoteeng00@gmail.com>
2022-08-10 22:19:10 +0700

    Update coding style

--- biothings/web/analytics/events.py
+++ biothings/web/analytics/events.py
@@ -1,14 +1,15 @@
 import hashlib
+
 # import smtplib
 import uuid
 from collections import UserDict
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from ipaddress import IPv4Address, IPv6Address, ip_address
+from pprint import pformat
 from random import randint
 from typing import Union
 from urllib.parse import urlencode
-from pprint import pformat
 
 
 class Event(UserDict):
@@ -30,12 +31,12 @@ class Event(UserDict):
         if self.user_agent:
             hash_val = 0
             for ordinal in map(ord, self.user_agent[::-1]):
-                hash_val = ((hash_val << 6) & 0xfffffff) + ordinal + (ordinal << 14)
-                left_most_7 = hash_val & 0xfe00000
+                hash_val = ((hash_val << 6) & 0xFFFFFFF) + ordinal + (ordinal << 14)
+                left_most_7 = hash_val & 0xFE00000
                 if left_most_7 != 0:
                     hash_val ^= left_most_7 >> 21
 
-        return ((randint(0, 0x7fffffff) ^ hash_val) & 0x7fffffff)
+        return (randint(0, 0x7FFFFFFF) ^ hash_val) & 0x7FFFFFFF
 
     def _cid_v2(self):
         # Author: Zhongchao Qian
@@ -58,7 +59,7 @@ class Event(UserDict):
             rip: Union[IPv4Address, IPv6Address] = ip_address(self.user_ip)
             ip_packed = rip.packed
         except ValueError:  # in the weird case I don't get an IP
-            ip_packed = randint(0, 0xffffffff).to_bytes(4, 'big')  # nosec
+            ip_packed = randint(0, 0xFFFFFFFF).to_bytes(4, 'big')  # nosec
 
         h = hashlib.blake2b(digest_size=16, salt=b'biothings')
         h.update(ip_packed)
@@ -66,8 +67,8 @@ class Event(UserDict):
 
         d = bytearray(h.digest())
         # truncating hash is not that bad, fixing some bits should be okay, too
-        d[6] = 0x40 | (d[6] & 0x0f)  # set version
-        d[8] = 0x80 | (d[8] & 0x3f)  # set variant
+        d[6] = 0x40 | (d[6] & 0x0F)  # set version
+        d[8] = 0x80 | (d[8] & 0x3F)  # set variant
         u = str(uuid.UUID(bytes=bytes(d)))
         return u
 
@@ -96,7 +97,7 @@ class Event(UserDict):
             "cid": self._cid(cid_version),
             "uip": self.user_ip,
             "dh": self.host,
-            "dp": self.path
+            "dp": self.path,
         }
 
         # add document referer
@@ -114,13 +115,15 @@ class Event(UserDict):
     def to_GA4_payload(self, measurement_id, cid_version=1):
         payload = {
             "name": "page_view",
-            "params": _clean({
-                "client_id": self._cid(cid_version),
-                "user_ip": self.user_ip,
-                "page_location": f'{self.host}{self.path}',
-                "page_title": self.path.strip('/').replace('/', '-') if self.path.strip('/') else 'index',
-                "page_path": self.path
-            })
+            "params": _clean(
+                {
+                    "client_id": self._cid(cid_version),
+                    "user_ip": self.user_ip,
+                    "page_location": f'{self.host}{self.path}',
+                    "page_title": self.path.strip('/').replace('/', '-'),
+                    "page_path": self.path,
+                }
+            ),
         }
 
         # add document referer
@@ -157,16 +160,22 @@ class GAEvent(Event):
 
         payloads = super().to_GA_payload(tracking_id, cid_version)
         if self.get("category") and self.get("action"):
-            payloads.append(urlencode(_clean({
-                "v": 1,  # protocol version
-                "t": "event",
-                "tid": tracking_id,
-                "cid": self._cid(cid_version),
-                "ec": self["category"],
-                "ea": self["action"],
-                "el": self.get("label", ""),
-                "ev": self.get("value", "")
-            })))
+            payloads.append(
+                urlencode(
+                    _clean(
+                        {
+                            "v": 1,  # protocol version
+                            "t": "event",
+                            "tid": tracking_id,
+                            "cid": self._cid(cid_version),
+                            "ec": self["category"],
+                            "ea": self["action"],
+                            "el": self.get("label", ""),
+                            "ev": self.get("value", ""),
+                        }
+                    )
+                )
+            )
         for event in self.get("__secondary__", []):
             event["__request__"] = self["__request__"]
             payloads.extend(event.to_GA_payload(tracking_id, cid_version)[1:])
@@ -178,15 +187,19 @@ class GAEvent(Event):
 
         payloads = super().to_GA4_payload(measurement_id, cid_version)
         if self.get("category") and self.get("action"):
-            payloads.append({
-                "name": self["action"],  # Event hit type
-                "params": _clean({
-                    "client_id": str(self._cid(cid_version)),  # Anonymous Client ID.
-                    "event_category": self["category"],  # Event Category. Required.
-                    "event_label": self.get("label", ""),  # Event label.
-                    "value": self.get("value", ""),  # Event value.
-                })
-            })
+            payloads.append(
+                {
+                    "name": self["action"],  # Event hit type
+                    "params": _clean(
+                        {
+                            "client_id": str(self._cid(cid_version)),  # Anonymous Client ID.
+                            "event_category": self["category"],  # Event Category. Required.
+                            "event_label": self.get("label", ""),  # Event label.
+                            "value": self.get("value", ""),  # Event value.
+                        }
+                    ),
+                }
+            )
         for event in self.get("__secondary__", []):
             event["__request__"] = self["__request__"]
             payloads.extend(event.to_GA4_payload(measurement_id, cid_version)[1:])
@@ -201,10 +214,11 @@ class Message(Event):
     Processable fields: title, body, url, url_text, image, image_altext
     Optionally define default field values below.
     """
+
     DEFAULTS = {
         "title": "Notification Message",
         "url_text": "View Details",
-        "image_altext": "<IMAGE>"
+        "image_altext": "<IMAGE>",
     }
 
     def __getattr__(self, attr):
@@ -222,24 +236,24 @@ class Message(Event):
         Generate ADF for Atlassian Jira payload. Overwrite this to build differently.
         https://developer.atlassian.com/cloud/jira/platform/apis/document/playground/
         """
-        adf = {
-            "version": 1,
-            "type": "doc",
-            "content": []
-        }
+        adf = {"version": 1, "type": "doc", "content": []}
         if self.body:
-            adf["content"].append({
-                "type": "paragraph",
-                "content": [{"type": "text", "text": self.body}]
-            })
+            adf["content"].append(
+                {"type": "paragraph", "content": [{"type": "text", "text": self.body}]}
+            )
         if self.url:
-            adf["content"].append({
-                "type": "paragraph",
-                "content": [{
-                    "type": "text", "text": self.url_text,
-                    "marks": [{"type": "link", "attrs": {"href": self.url}}]
-                }]
-            })
+            adf["content"].append(
+                {
+                    "type": "paragraph",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": self.url_text,
+                            "marks": [{"type": "link", "attrs": {"href": self.url}}],
+                        }
+                    ],
+                }
+            )
         return adf
 
     def to_slack_payload(self):
@@ -249,17 +263,17 @@ class Message(Event):
         """
         blocks = []
         if self.title:
-            blocks.append({
-                "type": "header",
-                "text": {
-                    "type": "plain_text",
-                    "text": ":sparkles: " + self.title,
-                    "emoji": True
+            blocks.append(
+                {
+                    "type": "header",
+                    "text": {
+                        "type": "plain_text",
+                        "text": ":sparkles: " + self.title,
+                        "emoji": True,
+                    },
                 }
-            })
-            blocks.append({
-                "type": "divider"
-            })
+            )
+            blocks.append({"type": "divider"})
         if self.body:
             body = {
                 "type": "section",
@@ -269,21 +283,17 @@ class Message(Event):
                 body["accessory"] = {
                     "type": "image",
                     "image_url": self.image,
-                    "alt_text": self.image_altext
+                    "alt_text": self.image_altext,
                 }
             blocks.append(body)
         if self.url:
-            blocks.append({
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": f"*<{self.url}|{self.url_text}>*"}
-            })
-        return {
-            "attachments": [{
-                "blocks": blocks
-            }]
-        }
+            blocks.append(
+                {
+                    "type": "section",
+                    "text": {"type": "mrkdwn", "text": f"*<{self.url}|{self.url_text}>*"},
+                }
+            )
+        return {"attachments": [{"blocks": blocks}]}
 
     def to_jira_payload(self, profile):
         """
@@ -299,7 +309,7 @@ class Message(Event):
                 "reporter": {"id": profile.reporter_id},
                 "priority": {"id": "3"},
                 "labels": [profile.label],
-                "description": self.to_ADF()
+                "description": self.to_ADF(),
             }
         }
 
@@ -320,7 +330,8 @@ class Message(Event):
 
         # Record the MIME types of both parts - text/plain and text/html.
         part1 = MIMEText(self.body, 'plain')
-        part2 = MIMEText(f"""
+        part2 = MIMEText(
+            f"""
         <!DOCTYPE html>
         <html>
             <head>
@@ -329,7 +340,9 @@ class Message(Event):
             <body>
                 <p>{self.body}</p>
             </body>
-        </html>""", 'html')
+        </html>""",
+            'html',
+        )
 
         # Attach parts into message container.
         # According to RFC 2046, the last part of a multipart message, in this case
