e2274f1f807583e87c2c45744f8d2f06510476ed
remoteeng00 <remoteeng00@gmail.com>
2022-12-19 23:57:15 +0700

    Use warning module to display message

--- biothings/hub/dataload/storage.py
+++ biothings/hub/dataload/storage.py
@@ -1,3 +1,7 @@
-# This module is deprecated, please directly import `biothings.utils.storage`
+import warnings
 
+warnings.filterwarnings("default", category=DeprecationWarning, module=__name__)
+warnings.warn(
+    "This module is deprecated, please use `biothings.utils.storage` module", DeprecationWarning
+)
 from biothings.utils.storage import *  # NOQA #F403
