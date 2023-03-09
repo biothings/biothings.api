7157d6c74d2be54477c4c96874fd3ebfadcc4806
everaldorodrigo <everaldorodrigo@gmail.com>
2023-03-08 17:26:36 -0800

    Add parameter to function.

--- tests/web/query/engine.py
+++ tests/web/query/engine.py
@@ -5,12 +5,13 @@ from pprint import pprint as print
 def test_adjust_index_overrided():
 
     class MyESQueryBackend(ESQueryBackend):
-        def adjust_index(self):
+        def adjust_index(self, query, **options):
             return 'any_index'
 
     client = connections.get_es_client("localhost:9200", True)
     backend = MyESQueryBackend(client)
-    index = backend.adjust_index()
+    query = None
+    index = backend.adjust_index(query)
     assert index == 'any_index'
 
 if __name__ == '__main__':
