9a4484d0823637abf8d1e98511df912b61f7983c
Chunlei Wu <anewgene@yahoo.com>
2023-04-09 15:47:18 -0700

    style: fourmat fixes for docs

--- docs/auto_rebuild.py
+++ docs/auto_rebuild.py
@@ -1,40 +1,41 @@
-'''
+"""
 A utility to monitor source tree and trigger the sphinx_build when changes are detected.
 
 Run it from the root of the src tree in a console as:
 
     python auto_rebuild.py
-'''
+"""
 import os.path
 import subprocess
 
 import tornado.autoreload
 import tornado.ioloop
 
-#an alternative:
-#watchmedo shell-command --pattern="*.rst;*.py" --recursive --command="make html" .
+# an alternative:
+# watchmedo shell-command --pattern="*.rst;*.py" --recursive --command="make html" .
 
-included_ext = ['.py', '.rst', '.css', '.html']
-doc_dir = './docs'
-src_dir = '.'
+included_ext = [".py", ".rst", ".css", ".html"]
+doc_dir = "./docs"
+src_dir = "."
 
 
 def build():
-    '''callback function when a src file is changed.'''
-    subprocess.call('make html'.split(), cwd=doc_dir)
+    """callback function when a src file is changed."""
+    subprocess.call("make html".split(), cwd=doc_dir)
     # restart dev server if needed (by touch a file)
     # subprocess.call('touch ../src/index.py'.split())
 
 
 def watch_src(src_path):
-    '''Watch src files with the specified file extensions.'''
-    for dirname, subdirs, fnames in os.walk(src_path):
+    """Watch src files with the specified file extensions."""
+    for dirname, _subdirs, fnames in os.walk(src_path):
         for fn in fnames:
             for ext in included_ext:
                 if fn.endswith(ext):
                     f_path = os.path.join(dirname, fn)
                     tornado.autoreload.watch(f_path)
                     # print f_path, os.path.exists(f_path)
+
 
 def main():
     tornado.autoreload.add_reload_hook(build)
@@ -44,5 +45,5 @@ def main():
     loop.start()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
