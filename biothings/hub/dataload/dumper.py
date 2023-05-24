2e2686ad6e980434b2047e15563ad9bdf37ec3d6
Chunlei Wu <anewgene@yahoo.com>
2023-05-24 10:29:39 -0700

    fix: another fix in set_keep_container logic

--- biothings/hub/dataload/dumper.py
+++ biothings/hub/dataload/dumper.py
@@ -1955,7 +1955,9 @@ class DockerContainerDumper(BaseDumper):
 
     def set_keep_container(self):
         self.KEEP_CONTAINER = (
-            self.source_config.get("keep_container", None) is None or self.image_metadata.get("keep_container", "").lower() in {"true", "yes", "1", "y"}  or False
+            self.source_config["keep_container"]
+            if "keep_container" in self.source_config
+            else self.image_metadata.get("keep_container", "").lower() in {"true", "yes", "1", "y"}
         )
 
     def set_get_version_cmd(self):
