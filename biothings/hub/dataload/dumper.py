07ad22b5170a9f926f515f37c6a7251c552a4b4d
Jason Lin <35415519+jal347@users.noreply.github.com>
2023-06-14 11:41:57 -0700

    Added comments for including DOCKER_HOST in config file

--- biothings/hub/dataload/dumper.py
+++ biothings/hub/dataload/dumper.py
@@ -1806,7 +1806,7 @@ class DockerContainerDumper(BaseDumper):
             - The Docker API on the remote server MUST BE secured with TLS
             - A TLS key pair is generated on the Hub server and placed inside the same data plugin folder or the data source folder
 
-    All info about Docker client connection MUST BE defined in the `config.py` file, under the DOCKER_CONFIG key, Ex:
+    All info about Docker client connection MUST BE defined in the `config.py` file, under the DOCKER_CONFIG and DOCKER_HOST key, Ex:
         DOCKER_CONFIG = {
             "CONNECTION_NAME_1": {
                 "tls_cert_path": "/path/to/cert.pem",
@@ -1820,6 +1820,7 @@ class DockerContainerDumper(BaseDumper):
                 "client_url": "unix://var/run/docker.sock"
             }
         }
+        DOCKER_HOST = None
 
     The data_url should match the following format:
         docker://CONNECTION_NAME?image=DOCKER_IMAGE&tag=TAG&path=/path/to/remote_file&dump_command="this is custom command"&container_name=CONTAINER_NAME&keep_container=true&get_version_cmd="cmd"  # NOQA
