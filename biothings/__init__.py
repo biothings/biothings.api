55b8353a4535d9f1a1e7f643aded89c2f4096e70
Chunlei Wu <anewgene@yahoo.com>
2023-07-05 11:50:41 -0700

    build: :building_construction: mark for 0.12.1 release

--- biothings/__init__.py
+++ biothings/__init__.py
@@ -8,7 +8,7 @@ class _version_info(NamedTuple):
     micro: int
 
 
-version_info = _version_info(0, 12, "0")
+version_info = _version_info(0, 12, "1")
 __version__ = ".".join(map(str, version_info))
 
 
