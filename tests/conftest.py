6ea14834daa101f43c2ad7f313b71eaabd7f5a47
ctrl-schaff <schaffjohnathan@gmail.com>
2024-01-02 10:52:52 -0800

    Add main fixture for copying test data

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,9 @@
-""" Ref: https://docs.pytest.org/en/latest/reference/fixtures.html#conftest-py-sharing-fixtures-across-multiple-files
+""" 
+Ref: https://docs.pytest.org/en/latest/reference/fixtures.html#conftest-py-sharing-fixtures-across-multiple-files
 
-    The conftest.py file serves as a means of providing fixtures for an entire directory.
-    Fixtures defined in a conftest.py can be used by any test in that package without needing to import them
-    (pytest will automatically discover them).
+The conftest.py file serves as a means of providing fixtures for an entire directory.
+Fixtures defined in a conftest.py can be used by any test in that package without needing to import them
+(pytest will automatically discover them).
 """
 
 # make sure hub is config before testing.
