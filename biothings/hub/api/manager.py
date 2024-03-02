c648cdfdfe6d8ed7702fad75e974bf8607076f1a
jal347 <linjason.03@gmail.com>
2024-03-01 17:13:51 -0800

    added await

--- biothings/hub/api/manager.py
+++ biothings/hub/api/manager.py
@@ -110,7 +110,7 @@ class APIManager(BaseManager):
             PYTEST_PATH = os.path.join(os.getcwd(), config_mod.PYTEST_PATH)
             pinfo = self.get_pinfo()
             pinfo["description"] = "Running API tests"
-            job = self.job_manager.defer_to_process(pinfo, partial(pytest.main, [PYTEST_PATH, "-m", "not userquery", "--host", "mygene.info"]))
+            job = await self.job_manager.defer_to_process(pinfo, partial(pytest.main, [PYTEST_PATH, "-m", "not userquery", "--host", "mygene.info"]))
             # pytest.main([PYTEST_PATH, "-m", "not userquery", "--host", str(port)])
 
             got_error = False
