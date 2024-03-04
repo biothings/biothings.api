1aaa8cb87a79e144ab85dc5a37f8cffbb9197261
jal347 <linjason.03@gmail.com>
2024-03-04 15:50:33 -0800

    test to see if logging works for pytest

--- biothings/hub/api/manager.py
+++ biothings/hub/api/manager.py
@@ -110,7 +110,7 @@ class APIManager(BaseManager):
             PYTEST_PATH = os.path.join(config_mod.PYTEST_PATH)
             pinfo = self.get_pinfo()
             pinfo["description"] = "Running API tests"
-            job = await self.job_manager.defer_to_process(pinfo, partial(pytest.main, [PYTEST_PATH, "-m", "not userquery", "--host", "mygene.info"]))
+            job = await self.job_manager.defer_to_process(pinfo, partial(pytest.main, ["-o", "log-cli=true", PYTEST_PATH, "-m", "not userquery", "--host", "mygene.info"]))
             # pytest.main([PYTEST_PATH, "-m", "not userquery", "--host", str(port)])
 
             got_error = False
