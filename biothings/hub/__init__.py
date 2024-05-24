d39cd797e99e4fde805b4bde3eaae5fb1515c7af
Jason Lin <35415519+jal347@users.noreply.github.com>
2024-05-24 10:48:32 -0700

    Api customization (#336)
    Initial implementation of allowing use of custom config_webs when creating apis using the hub.
    
    ---------
    
    Co-authored-by: mygene_hub <mygene@su05.scripps.edu>
    Co-authored-by: Everaldo <everaldorodrigo@gmail.com>
    Co-authored-by: Dylan Welzel <dylanwelzel@gmail.com>

--- biothings/hub/__init__.py
+++ biothings/hub/__init__.py
@@ -147,6 +147,7 @@ DIFFER_CATEGORY = "differ"
 DIFFMANAGER_CATEGORY = "diffmanager"
 SYNCER_CATEGORY = "syncer"
 INSPECTOR_CATEGORY = "inspector"
+APITESTER_CATEGORY = "apitester"
 
 # HUB_REFRESH_COMMANDS = hasattr(
 #     config, "HUB_REFRESH_COMMANDS"
@@ -759,7 +760,8 @@ class HubServer(object):
         from biothings.hub.api.manager import APIManager
 
         args = self.mixargs("api")
-        api_manager = APIManager(**args)
+        api_manager = APIManager(job_manager=self.managers["job_manager"], **args)
+        # api_manager = APIManager(**args)
         self.managers["api_manager"] = api_manager
 
     def configure_source_manager(self):
@@ -1292,7 +1294,8 @@ class HubServer(object):
             self.extra_commands["delete_api"] = CommandDefinition(command=self.managers["api_manager"].delete_api)
             self.extra_commands["create_api"] = CommandDefinition(command=self.managers["api_manager"].create_api)
             self.extra_commands["start_api"] = CommandDefinition(command=self.managers["api_manager"].start_api)
-            self.extra_commands["stop_api"] = self.managers["api_manager"].stop_api
+            self.extra_commands["stop_api"] = CommandDefinition(command=self.managers["api_manager"].stop_api)
+            self.extra_commands["test_api"] = CommandDefinition(command=self.managers["api_manager"].test_api)
         if "upgrade" in self.DEFAULT_FEATURES:
 
             def upgrade(code_base):  # just a wrapper over dumper
@@ -1502,6 +1505,8 @@ class HubServer(object):
             )
         if "create_api" in cmdnames:
             self.api_endpoints["api"].append(EndpointDefinition(name="create_api", method="post", force_bodyargs=True))
+        if "test_api" in cmdnames:
+            self.api_endpoints["api"].append(EndpointDefinition(name="test_api", method="post", suffix="test"))
         if not self.api_endpoints["api"]:
             self.api_endpoints.pop("api")
         if "get_apis" in cmdnames:
