b3a1a009bf08ce53831e81ede0915f97e910bbb4
jschaff <jschaff@scripps.edu>
2024-08-20 10:05:59 -0700

    Move the tests to the corresponding tests directory

--- tests/hub/dataindex/test_scheduler.py
+++ tests/hub/dataindex/test_scheduler.py
@@ -10,15 +10,12 @@ def test_schedule_iteration(total: int, batch_size: int):
     expected structure of the Schedule object
     """
     schedule = Schedule(total, batch_size)
-    # schedule.__next__()
-    # self._state = self.suffix("running, on batch") + ","
 
     batch_count = 1
     for batch in schedule:
         assert batch_count == batch
         batch_count += 1
-        print(schedule)
-        print(schedule.suffix("Task"))
 
-
-
+        suffix_value = "Task"
+        suffix_repr = f"{suffix_value} #{schedule._batch}/{schedule._batches} {schedule._percentage}"
+        assert suffix_repr == schedule.suffix(suffix_value)
