dc690115011a1946481b9708839575c8b581caa2
Everaldo <everaldorodrigo@gmail.com>
2024-08-26 16:02:14 -0700

    Create setup.py and remove pyproject.toml

---
+++ biothings/tests/biothings_pytest_plugin/setup.py
@@ -0,0 +1,32 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+if os.path.exists('README.md'):
+    long_description = open('README.md').read()
+else:
+    long_description = "Project description not available."
+
+setup(
+    name='biothings_pytest_plugin',
+    version='0.1',
+    entry_points={
+        'pytest11': [
+            'biothings-pytest-plugin = biothings_pytest_plugin',
+        ],
+    },
+    description='A pytest plugin for biothings',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/biothings/biothings.api/tree/master/biothings/tests/biothings_pytest_plugin',
+    author='The BioThings Team',
+    author_email='cwu@scripps.edu',
+    license='Apache Software License (Apache License, Version 2.0)',
+    install_requires=[
+        'pytest',
+        'boto3',
+        'requests',
+    ],
+    python_requires='>=3.8',
+)
