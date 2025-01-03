685356fc659e5f95d87b28cf392a9072b178c5da
jschaff <jschaff@scripps.edu>
2025-01-03 08:50:39 -0800

    Change the settings to use localhost:9200 for es

--- biothings/cli/settings.py
+++ biothings/cli/settings.py
@@ -113,7 +113,7 @@ def setup_biothings_configuration():
         "indexer_select": {},
         "env": {
             "localhub": {
-                "host": "http://localhost:27017",
+                "host": "http://localhost:9200",
                 "indexer": {"args": {"request_timeout": 300, "retry_on_timeout": True, "max_retries": 10}},
             }
         },
