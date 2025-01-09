daa54fba6c968363a9d46f1687df07456daa8776
jschaff <jschaff@scripps.edu>
2025-01-09 10:13:50 -0800

    Working commit for the schema display

--- biothings/cli/manifest.py
+++ biothings/cli/manifest.py
@@ -1,12 +1,14 @@
 import json
 from pathlib import Path
-from typing import Union
 
+import jsonschema
 import typer
+from rich import box, print as rprint
+from rich.console import Console
+from rich.panel import Panel
+from rich.table import Table
 
 from biothings.cli import utils
-from biothings.hub.dataplugin.loaders.loader import ManifestBasedPluginLoader
-from biothings.hub.dataplugin.loads.schema import load_manifest_schema
 
 logger = utils.get_logger("biothings-cli")
 
@@ -31,8 +33,29 @@ def display_schema():
 
     Displays the schema contents storied in the biothings project
     """
+    from biothings.hub.dataplugin.loaders.schema import load_manifest_schema
+
     manifest_schema = load_manifest_schema()
-    breakpoint()
+    schema_validator = jsonschema.validators.validator_for(manifest_schema)
+    valid_schema = False
+    try:
+        schema_validator.check_schema(manifest_schema)
+        valid_schema = True
+    except jsonschema.exceptions.SchemaError as schema_error:
+        logger.exception(schema_error)
+
+    schema_repr = json.dumps(manifest_schema, indent=2)
+
+    console = Console()
+    panel = Panel(
+        "[bold green]Schema Information[/bold green]\n"
+        f"* [italic]Valid Schema[/italic]: {valid_schema}\n"
+        f"* [italic]Schema Contents[/italic]:\n{schema_repr}",
+        title="[bold green]Biothings[JSONSchema][/bold green]",
+        subtitle="[bold green]Biothings[JSONSchema][/bold green]",
+        box=box.ASCII,
+    )
+    console.print(panel)
 
 
 @manifest_application.command(
@@ -40,12 +63,15 @@ def display_schema():
     help="Validates a provided manfiest file against the json schema",
     no_args_is_help=True,
 )
-def validate_manifest(manifest_file: Union[str, Path]) -> None:
+def validate_manifest(manifest_file: str) -> None:
     """
     *validate* command
 
     Displays the schema contents storied in the biothings project
     """
+    from biothings.hub.dataplugin.loaders.loader import ManifestBasedPluginLoader
+
+    manifest_file = Path(manifest_file).resolve().absolute()
     plugin_name = "ManifestValidation"
     manifest_loader = ManifestBasedPluginLoader(plugin_name=plugin_name)
     with open(manifest_file, "r", encoding="utf-8") as manifest_handle:
