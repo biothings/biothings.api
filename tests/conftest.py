039ce1f67b78c57c4294f93ddf5a3946c7df3901
ctrl-schaff <59380780+ctrl-schaff@users.noreply.github.com>
2025-01-23 10:32:19 -0800

    Remove cgi dependency (#379)
    * Replace the cgi module in the HTTPDumper class
    
    * Remove the cgi dependency from the UploadHandler
    
    ---------
    
    Co-authored-by: jschaff <jschaff@scripps.edu>

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@
     Fixtures defined in a conftest.py can be used by any test in that package without needing to import them
     (pytest will automatically discover them).
 """
+
 import sys
 import os.path
 
@@ -19,6 +20,7 @@ config_mod.ES_HOST = "http://localhost:9200"  # optional
 config_mod.ES_INDICES = {"dev": "main_build_configuration"}
 config_mod.ANNOTATION_DEFAULT_SCOPES = ["_id", "symbol"]
 config_mod.LOG_FOLDER = os.path.join(config_mod.DATA_ARCHIVE_ROOT, "logs")
+config_mod.DATA_SRC_DATABASE = "src.db"
 
 sys.modules["config"] = config_mod
 sys.modules["biothings.config"] = config_mod
--- tests/hub/dataload/unlist.py
+++ tests/hub/dataload/unlist.py
@@ -2,14 +2,15 @@ import unittest
 
 from biothings.utils.dataload import unlist
 
+
 class TestUnlistFunction(unittest.TestCase):
     def test_single_element_list(self):
         """
         Test unlisting a single element list.
         :return:
         """
-        data = {'key': ['value']}
-        expected = {'key': 'value'}
+        data = {"key": ["value"]}
+        expected = {"key": "value"}
         self.assertEqual(unlist(data), expected)
 
     def test_single_element_nested_list(self):
@@ -17,8 +18,8 @@ class TestUnlistFunction(unittest.TestCase):
         Test unlisting a single element nested list.
         :return:
         """
-        data = {'key': [{'subkey': ['subvalue']}]}
-        expected = {'key': {'subkey': 'subvalue'}}
+        data = {"key": [{"subkey": ["subvalue"]}]}
+        expected = {"key": {"subkey": "subvalue"}}
         self.assertEqual(unlist(data), expected)
 
     def test_multiple_element_list(self):
@@ -26,8 +27,8 @@ class TestUnlistFunction(unittest.TestCase):
         Test that a multiple element list is not unlisted.
         :return:
         """
-        data = {'key': ['value1', 'value2']}
-        expected = {'key': ['value1', 'value2']}
+        data = {"key": ["value1", "value2"]}
+        expected = {"key": ["value1", "value2"]}
         self.assertEqual(unlist(data), expected)
 
     def test_nested_dict_with_single_element_list(self):
@@ -35,8 +36,8 @@ class TestUnlistFunction(unittest.TestCase):
         Test unlisting a nested dictionary with single element list.
         :return:
         """
-        data = {'gene': [{'id': 1017, 'source': ['clinvar']}, {'id': 1018, 'source': ['bg']}]}
-        expected = {'gene': [{'id': 1017, 'source': 'clinvar'}, {'id': 1018, 'source': 'bg'}]}
+        data = {"gene": [{"id": 1017, "source": ["clinvar"]}, {"id": 1018, "source": ["bg"]}]}
+        expected = {"gene": [{"id": 1017, "source": "clinvar"}, {"id": 1018, "source": "bg"}]}
         self.assertEqual(unlist(data), expected)
 
     def test_nested_dict_with_multiple_element_list(self):
@@ -44,8 +45,8 @@ class TestUnlistFunction(unittest.TestCase):
         Test nested dictionary with multiple element list, ensuring no unlisting.
         :return:
         """
-        data = {'gene': [{'id': 1017, 'source': ['clinvar', 'dbsnp']}, {'id': 1018, 'source': ['bg']}]}
-        expected = {'gene': [{'id': 1017, 'source': ['clinvar', 'dbsnp']}, {'id': 1018, 'source': 'bg'}]}
+        data = {"gene": [{"id": 1017, "source": ["clinvar", "dbsnp"]}, {"id": 1018, "source": ["bg"]}]}
+        expected = {"gene": [{"id": 1017, "source": ["clinvar", "dbsnp"]}, {"id": 1018, "source": "bg"}]}
         self.assertEqual(unlist(data), expected)
 
     def test_empty_list(self):
@@ -53,8 +54,8 @@ class TestUnlistFunction(unittest.TestCase):
         Test that an empty list remains unchanged.
         :return:
         """
-        data = {'key': []}
-        expected = {'key': []}
+        data = {"key": []}
+        expected = {"key": []}
         self.assertEqual(unlist(data), expected)
 
     def test_no_list(self):
@@ -62,8 +63,8 @@ class TestUnlistFunction(unittest.TestCase):
         Test that a value without a list remains unchanged.
         :return:
         """
-        data = {'key': 'value'}
-        expected = {'key': 'value'}
+        data = {"key": "value"}
+        expected = {"key": "value"}
         self.assertEqual(unlist(data), expected)
 
     def test_deeply_nested_single_element_list(self):
@@ -71,8 +72,8 @@ class TestUnlistFunction(unittest.TestCase):
         Test unlisting a deeply nested single element list.
         :return:
         """
-        data = {'key': {'subkey': [{'subsubkey': ['subsubvalue']}]} }
-        expected = {'key': {'subkey': {'subsubkey': 'subsubvalue'}} }
+        data = {"key": {"subkey": [{"subsubkey": ["subsubvalue"]}]}}
+        expected = {"key": {"subkey": {"subsubkey": "subsubvalue"}}}
         self.assertEqual(unlist(data), expected)
 
     def test_no_change_needed(self):
@@ -80,10 +81,10 @@ class TestUnlistFunction(unittest.TestCase):
         Test that no changes are made if no unlisting is needed.
         :return:
         """
-        data = {'key': {'subkey': 'subvalue'}}
-        expected = {'key': {'subkey': 'subvalue'}}
+        data = {"key": {"subkey": "subvalue"}}
+        expected = {"key": {"subkey": "subvalue"}}
         self.assertEqual(unlist(data), expected)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
