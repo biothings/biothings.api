8add405445e4f48a54c994d316d3d1f7e467b6e8
jschaff <jschaff@scripps.edu>
2025-02-13 10:52:43 -0800

    Separate the web server start commands ...
    Separates the command for starting the hub web api
    and the ssh hub. Allows for easier testing of the
    specific hub server when making a function call.
    
    Also utilizes self.features in place of the class property
    DEFAULT_FEATURES when configuring the hub server so that
    certain features aren't assumed to be required when they were
    not passed. Useful when making a minimal hub server for testing
    purposes

--- biothings/hub/__init__.py
+++ biothings/hub/__init__.py
@@ -398,21 +398,24 @@ class HubServer(object):
         can be used to customize reloader, dataupload and websocket. If None, default config
         is used. If explicitely False, feature is deactivated.
         """
-        managers_custom_args = managers_custom_args or {}
+
+        if managers_custom_args is None:
+            managers_custom_args = {}
+        self.managers_custom_args = managers_custom_args
+
         self.name = name
         self.source_list = source_list
         self.logger, self.logfile = get_logger("hub")
-        self._passed_features = features
-        self._passed_managers_custom_args = managers_custom_args
         self.features = self.clean_features(features or self.DEFAULT_FEATURES)
-        self.managers_custom_args = managers_custom_args
         self.reloader_config = reloader_config or self.DEFAULT_RELOADER_CONFIG
         self.dataupload_config = dataupload_config or self.DEFAULT_DATAUPLOAD_CONFIG
         self.websocket_config = websocket_config or self.DEFAULT_WEBSOCKET_CONFIG
         self.autohub_config = autohub_config or self.DEFAULT_AUTOHUB_CONFIG
+
         # collect listeners that should be connected (push data through) to websocket
         self.ws_listeners = []
         self.api_config = api_config or self.DEFAULT_API_CONFIG
+
         # set during configure()
         self.managers = None
         self.api_endpoints = {}
@@ -535,14 +538,10 @@ class HubServer(object):
     def before_start(self):
         pass
 
-    def start(self):
+    def start_web_api_server(self):
         if not self.configured:
             self.configure()
         self.logger.info("Starting '%s'", self.name)
-        # can't use asyncio.get_event_loop() if python < 3.5.3 as it would return
-        # another instance of aio loop, take it from job_manager to make sure
-        # we share the same one
-        loop = self.managers["job_manager"].loop
         if self.routes:
             self.logger.info("Starting Hub API server on port %s" % config.HUB_API_PORT)
             # self.logger.info(self.routes)
@@ -570,6 +569,10 @@ class HubServer(object):
                     )
         else:
             self.logger.info("No route defined, API server won't start")
+
+    def start(self):
+        loop = self.managers["job_manager"].loop
+        self.start_web_api_server()
         # at this point, everything is ready/set, last call for customizations
         self.before_start()
         self.logger.info("Starting Hub SSH server on port %s" % config.HUB_SSH_PORT)
@@ -1159,7 +1162,8 @@ class HubServer(object):
             self.commands["export_plugin"] = partial(self.managers["assistant_manager"].export)
         if self.managers.get("dataplugin_manager"):
             self.commands["dump_plugin"] = self.managers["dataplugin_manager"].dump_src
-        if "autohub" in self.DEFAULT_FEATURES:
+
+        if "autohub" in self.features:
             self.commands["list"] = CommandDefinition(command=self.autohub_feature.list_biothings, tracked=False)
             # dump commands
             self.commands["versions"] = partial(self.managers["dump_manager"].call, method_name="versions")
@@ -1297,7 +1301,8 @@ class HubServer(object):
             self.extra_commands["start_api"] = CommandDefinition(command=self.managers["api_manager"].start_api)
             self.extra_commands["stop_api"] = CommandDefinition(command=self.managers["api_manager"].stop_api)
             self.extra_commands["test_api"] = CommandDefinition(command=self.managers["api_manager"].test_api)
-        if "upgrade" in self.DEFAULT_FEATURES:
+
+        if "upgrade" in self.features:
 
             def upgrade(code_base):  # just a wrapper over dumper
                 """Upgrade (git pull) repository for given code base name ("biothings_sdk" or "application")"""
