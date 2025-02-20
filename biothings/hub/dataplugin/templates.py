378bb180ecfe0cd237e39deddde36a5a7a2c5bc0
jschaff <jschaff@scripps.edu>
2025-02-19 16:05:53 -0800

    Add the metaclass to globals stack

--- biothings/hub/dataplugin/templates.py
+++ biothings/hub/dataplugin/templates.py
@@ -23,7 +23,7 @@ from biothings.hub.dataload.uploader import AssistedUploader
 
 def generate_assisted_dumper_class(template_file: Union[str, Path], dumper_configuration: dict):
     with open(template_file, "r", encoding="utf-8") as tpl_handle:
-        template = Template(tpl_handle)
+        template = Template(tpl_handle.read())
         pystr = template.substitute(dumper_configuration)
 
         code = compile(pystr, "<string>", "exec")
@@ -33,18 +33,20 @@ def generate_assisted_dumper_class(template_file: Union[str, Path], dumper_confi
         dklass = getattr(mod, dumper_configuration["DUMPER_NAME"], None)
 
         # we need to inherit from a class here in this file so it can be pickled
+        metadumper_class = f"AssistedDumper_{dumper_configuration['SRC_NAME']}"
         assisted_dumper_class = type(
-            f"AssistedDumper_{dumper_configuration['SRC_NAME']}",
+            metadumper_class,
             (AssistedDumper, dklass),
             {},
         )
         assisted_dumper_class.python_code = pystr
+        globals()[metadumper_class] = assisted_dumper_class
         return assisted_dumper_class
 
 
 def generate_assisted_uploader_class(template_file: Union[str, Path], uploader_configuration: dict):
     with open(template_file, "r", encoding="utf-8") as tpl_handle:
-        tpl = Template(tpl_handle)
+        tpl = Template(tpl_handle.read())
         pystr = tpl.substitute(uploader_configuration)
 
         code = compile(pystr, "<string>", "exec")
@@ -53,13 +55,21 @@ def generate_assisted_uploader_class(template_file: Union[str, Path], uploader_c
         )
         mod = importlib.util.module_from_spec(spec)
         exec(code, mod.__dict__, mod.__dict__)
+
+        uploader_name = (
+            uploader_configuration["SRC_NAME"].capitalize() + uploader_configuration["SUB_SRC_NAME"] + "Uploader"
+        )
         uklass = getattr(mod, uploader_name, None)
 
         # we need to inherit from a class here in this file so it can be pickled
+        metauploader_class = (
+            f"AssistedUploader_{uploader_configuration['SRC_NAME'] + uploader_configuration['SUB_SRC_NAME']}"
+        )
         assisted_uploader_class = type(
-            "AssistedUploader_%s" % uploader_configuration["SRC_NAME"] + uploader_configuration["SUB_SRC_NAME"],
+            metauploader_class,
             (AssistedUploader, uklass),
             {},
         )
         assisted_uploader_class.python_code = pystr
+        globals()[metauploader_class] = assisted_uploader_class
         return assisted_uploader_class
