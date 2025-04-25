0e06895e19d4ce4f7781c56edfed07768ef90c74
ctrl-schaff <schaffjohnathan@gmail.com>
2025-04-25 14:46:36 -0700

    Web handlers fixture configuration update ...
    Removes the custom elasticsearch fixture to leverage the
    elasticsearch setup provided by BiothingsWebAppTest. Corrects the
    userquery data directory path and creates a fixture for it

--- tests/web/handlers/conftest.py
+++ tests/web/handlers/conftest.py
@@ -7,13 +7,9 @@ Manage sample dataset and mapping for testing
 
 """
 import copy
-import json
-import os
 import pathlib
 import sys
-import warnings
 
-import elasticsearch
 import pytest
 
 from biothings.utils.common import DummyConfig
@@ -46,15 +42,33 @@ class DefautlAPIHandler(BaseAPIHandler):
         self.finish(res)
 
 
+@pytest.fixture(scope="module")
+def userquery() -> pathlib.Path:
+    """
+    Generates the userquery data directory required for testing
+    """
+    data_directory = pathlib.Path(__file__).parent.joinpath("data")
+    userquery_directory = data_directory.joinpath("userquery")
+    assert userquery_directory.exists()
+
+    filter_data = userquery_directory / "exrna/filter.txt"
+    assert filter_data.exists()
+
+    query_data = userquery_directory / "prefix/query.txt"
+    assert query_data.exists()
+
+    return userquery_directory
+
+
 @pytest.fixture(scope="module", autouse=True)
-def handler_configuration():
+def handler_configuration(userquery: pathlib.Path):
+
     config_mod = DummyConfig(name="config")
 
     # *****************************************************************************
     # Elasticsearch Variables
     # *****************************************************************************
-    config_mod.ES_INDEX = TEST_INDEX
-    config_mod.ES_INDICES = {None: "_all", TEST_DOC_TYPE: TEST_INDEX}
+    config_mod.ES_INDICES = {TEST_DOC_TYPE: TEST_INDEX}
     config_mod.ES_DOC_TYPE = TEST_DOC_TYPE
     config_mod.ES_SCROLL_SIZE = 60
 
@@ -72,7 +86,8 @@ def handler_configuration():
     # *****************************************************************************
     config_mod.ALLOW_RANDOM_QUERY = True
     config_mod.ALLOW_NESTED_AGGS = True
-    config_mod.USERQUERY_DIR = os.path.join(os.path.dirname(__file__), "userquery")
+
+    config_mod.USERQUERY_DIR = userquery_directory
     config_mod.LICENSE_TRANSFORM = {"interpro": "pantherdb", "pantherdb.ortholog": "pantherdb"}  # For testing only.
 
     # *****************************************************************************
@@ -100,64 +115,3 @@ def handler_configuration():
     yield config_mod
     sys.modules["config"] = prior_config
     sys.modules["biothings.config"] = prior_biothings_config
-
-
-@pytest.fixture(scope="module")
-def handler_data_storage() -> dict:
-    test_directory = pathlib.Path(__file__).resolve().absolute().parent
-    data_location = test_directory.joinpath("data")
-    file_storage_mapping = {
-        "test_data.ndjson": data_location.joinpath("test_data.ndjson"),
-        "test_data_index.json": data_location.joinpath("test_data_index.json"),
-        "test_data_index_for_es8.json": data_location.joinpath("test_data_index_for_es8.json"),
-        "test_data_query.json": data_location.joinpath("test_data_query.json"),
-    }
-    return file_storage_mapping
-
-
-@pytest.fixture(scope="module", autouse=True)
-def setup_es(handler_data_storage: dict, handler_configuration):
-    """
-    Populate ES with test index and documents.
-    Index to localhost:9200 only.
-    """
-
-    client = elasticsearch.Elasticsearch(
-        hosts=TEST_HOST,
-    )
-
-    server_major_version = client.info()["version"]["number"].split(".")[0]
-
-    # if server_major_version != client_major_version:
-    #     pytest.exit('ES version does not match its python library.')
-
-    if not client.indices.exists(index=TEST_INDEX):
-
-        mapping_file = handler_data_storage["test_data_index.json"]
-        if int(server_major_version) >= 8:
-            mapping_file = handler_data_storage["test_data_index_for_es8.json"]
-
-        with open(str(mapping_file), "r", encoding="utf-8") as file:
-            mapping = json.load(file)
-
-        with open(str(handler_data_storage["test_data.ndjson"]), "r", encoding="utf-8") as file:
-            ndjson = file.read()
-
-        if int(server_major_version) >= 8:
-            client.indices.create(index=TEST_INDEX, **mapping)
-            client.bulk(body=ndjson, index=TEST_INDEX)
-        elif elasticsearch.__version__[0] > 6:
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore")
-                client.indices.create(index=TEST_INDEX, include_type_name=True, **mapping)
-                client.bulk(body=ndjson, index=TEST_INDEX)
-        else:
-            client.indices.create(index=TEST_INDEX, **mapping)
-            client.bulk(body=ndjson, index=TEST_INDEX, doc_type=TEST_DOC_TYPE)
-
-        client.indices.refresh()
-        yield
-        client.indices.delete(index=TEST_INDEX)
-
-    else:
-        yield
