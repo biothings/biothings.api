fcd5e89d21c95173bebd802ceaf9c8070f55c395
Chunlei Wu <anewgene@yahoo.com>
2025-05-01 16:58:10 -0700

    build: :package: switch to use git module intead
    replace the use of call_subprocess to call external git command.

--- setup.py
+++ setup.py
@@ -1,28 +1,38 @@
-from subprocess import CalledProcessError, check_output
+import logging
+import os.path
 
+from git import GitCommandError, InvalidGitRepositoryError, NoSuchPathError, Repo
 from setuptools import setup
 
 REPO_URL = "https://github.com/biothings/biothings.api"
 
-# should fail if installed from source or from pypi,
-# version gets set to MAJOR.MINOR.# commits on master branch if installed from pip repo
-# otherwise to MAJOR.MINOR.MICRO as defined in biothings.version
-try:
-    command = ["git", "rev-list", "--count", "master"]
-    NUM_COMMITS = check_output(command).strip().decode("utf-8")
-except CalledProcessError:
-    NUM_COMMITS = ""
 
-# Calculate commit hash, should fail if installed from source or from pypi
-try:
-    command = ["git", "rev-parse", "HEAD"]
-    COMMIT_HASH = check_output(command).strip().decode("utf-8")
-except CalledProcessError:
-    COMMIT_HASH = ""
+def get_git_info():
+    """
+    Get the git commit hash and number of commits in the current repository.
+    Returns a tuple (commit_hash, num_commits).
+    """
+    folder = os.path.dirname(__file__)
+    commit_hash, num_commits = "", ""
+    try:
+        repo = Repo(folder)
+        commit_hash = repo.head.object.hexsha
+        num_commits = repo.rev_parse("master").count()
+    except (InvalidGitRepositoryError, NoSuchPathError):
+        logging.warning("Not a valid git repository for folder '%s', skipped for checking new version.", folder)
+    except GitCommandError:
+        logging.exception("Error while getting git information for '%s'", folder)
+    except Exception as err:
+        logging.debug("Can't determine source code info (but that's fine) for: %s", err)
+
+    return commit_hash, num_commits
+
+
+COMMIT_HASH, NUM_COMMITS = get_git_info()
 
 # Write commit to file inside package, that can be read later
 if COMMIT_HASH or NUM_COMMITS:
-    with open("biothings/.git-info", "w", encoding="utf-8") as git_file:
+    with open(os.path.join(os.path.dirname(__file__), "biothings/.git-info"), "w", encoding="utf-8") as git_file:
         git_file.write(f"{REPO_URL}.git\n{COMMIT_HASH}\n{NUM_COMMITS}")
 
 setup(
