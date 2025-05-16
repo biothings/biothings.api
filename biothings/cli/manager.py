2ec1aa0a1085efe094ba3f57c644f545bdb57009
ctrl-schaff <59380780+ctrl-schaff@users.noreply.github.com>
2025-05-16 09:47:08 -0700

    Isolate cli job management (#405)
    * Revert to using CLIJobManager ...
    
    Due to the "spawn" versus "fork" paradigm,
    we want to ensure that the command-line has wide support
    over full parity of the hub. So until we re-factor the
    hub configuration to support changing the multiprocessing
    paradigm, we'll leverage a simplified versus of the JobManager
    
    * Remove duplicate -m shorthand arg for inspect command
    
    * Add support for multiple uploaders to index command ...
    
    Unexpected found this while testing the tutorials plugin, but
    the index command never handle multiple uploaders appropriately.
    
    We needed to add status registration calls to the upload process
    so that the upload state would be created in the biothing_hubdb.src_dump
    table / collection. After that the plugin_name.subsource_name would be
    resolvable via the `get_source_fullname` function that attempts
    to look under `upload.jobs` field within the biothings_hubdb.src_dump
    entries
    
    Added additional error handling to this and the inspect command to
    ensure parity between the two with messaging, along with producing more
    user friendly output upon forgetting to provide --sub-source-name
    
    We also build a different job manager for the indexing task
    
    * Add warning for non-MongoDB usage with index command

---
+++ biothings/cli/manager.py
@@ -0,0 +1,38 @@
+"""
+Temporary stopgap to leverage this minimal instance
+due to our limitation in the multiprocessing starting
+method
+
+Once we support setting new processes to spawn instead of fork,
+we can eliminate this manager
+"""
+
+from biothings.utils.common import get_loop
+
+
+class CLIJobManager:
+    """This is the minimal JobManager used in CLI mode to run async jobs, with the compatible methods as JobManager.
+    It won't use a dedicated ProcessPool or ThreadPool, and will just run async job directly in the asyncio loop
+    (which runs jobs in threads by default).
+    """
+
+    def __init__(self, loop=None):
+        self.loop = loop or get_loop()
+
+    async def defer_to_process(self, pinfo=None, func=None, *args, **kwargs):
+        """keep the same signature as JobManager.defer_to_process. The passed pinfo is ignored.
+        defer_to_process will still run func in the thread using defer_to_thread method.
+        """
+        fut = await self.defer_to_thread(pinfo, func, *args, **kwargs)
+        return fut
+
+    async def defer_to_thread(self, pinfo=None, func=None, *args):
+        """keep the same signature as JobManager.defer_to_thread. The passed pinfo is ignored"""
+
+        async def run(fut, func):
+            res = func()
+            fut.set_result(res)
+
+        fut = self.loop.create_future()
+        self.loop.create_task(run(fut, func))
+        return fut
